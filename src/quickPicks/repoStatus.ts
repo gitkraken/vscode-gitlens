1fb2826f5cf723ef6856978b11c78e02071aa361
Eric Amodio <eamodio@gmail.com>
2017-03-22 15:51:45 -0400

    Fixes pluralization

--- src/quickPicks/repoStatus.ts
+++ src/quickPicks/repoStatus.ts
@@ -115,7 +115,7 @@ export class RepoStatusQuickPick {
 
         if (status.upstream && status.state.ahead) {
             items.splice(0, 0, new CommandQuickPickItem({
-                label: `$(cloud-upload)\u00a0 ${status.state.ahead} Commits ahead of \u00a0$(git-branch) ${status.upstream}`,
+                label: `$(cloud-upload)\u00a0 ${status.state.ahead} Commit${status.state.ahead > 1 ? 's' : ''} ahead of \u00a0$(git-branch) ${status.upstream}`,
                 description: `\u00a0 \u2014 \u00a0\u00a0 shows commits in \u00a0$(git-branch) ${status.branch} but not \u00a0$(git-branch) ${status.upstream}`
             }, Commands.ShowQuickBranchHistory, [
                     new GitUri(Uri.file(status.repoPath), { fileName: '', repoPath: status.repoPath, sha: `${status.upstream}..${status.branch}` }),
@@ -131,7 +131,7 @@ export class RepoStatusQuickPick {
 
         if (status.upstream && status.state.behind) {
             items.splice(0, 0, new CommandQuickPickItem({
-                label: `$(cloud-download)\u00a0 ${status.state.behind} Commits behind \u00a0$(git-branch) ${status.upstream}`,
+                label: `$(cloud-download)\u00a0 ${status.state.behind} Commit${status.state.behind > 1 ? 's' : ''} behind \u00a0$(git-branch) ${status.upstream}`,
                 description: `\u00a0 \u2014 \u00a0\u00a0 shows commits in \u00a0$(git-branch) ${status.upstream} but not \u00a0$(git-branch) ${status.branch} (since \u00a0$(git-commit) ${status.sha.substring(0, 8)})`
             }, Commands.ShowQuickBranchHistory, [
                     new GitUri(Uri.file(status.repoPath), { fileName: '', repoPath: status.repoPath, sha: `${status.sha}..${status.upstream}` }),
