55cd3ce2f367f1ac3111c301469c3a7a4ca5b12b
rebornix <penn.lv@gmail.com>
2017-06-13 17:17:00 -0700

    extract stash explorer

---
+++ src/views/gitStashExplorer.ts
@@ -0,0 +1,43 @@
+'use strict';
+import { Event, EventEmitter, ExtensionContext, TreeDataProvider, TreeItem, Uri, window } from 'vscode';
+import { ExplorerNode, StashNode } from './gitExplorerNodes';
+import { GitService, GitUri } from '../gitService';
+import { StashCommitNode } from './stashCommitNode';
+
+export * from './gitExplorerNodes';
+
+export class GitStashExplorer implements TreeDataProvider<ExplorerNode>  {
+    private _node: ExplorerNode;
+    private _onDidChangeTreeData = new EventEmitter<ExplorerNode>();
+    public get onDidChangeTreeData(): Event<StashCommitNode> {
+        return this._onDidChangeTreeData.event;
+    }
+
+    constructor(private context: ExtensionContext, private git: GitService) {
+        const editor = window.activeTextEditor;
+
+        const uri = (editor !== undefined && editor.document !== undefined)
+            ? new GitUri(editor.document.uri, { repoPath: git.repoPath, fileName: editor.document.uri.fsPath })
+            : new GitUri(Uri.file(git.repoPath), { repoPath: git.repoPath, fileName: git.repoPath });
+
+        this._node = new StashNode(uri, this.context, this.git);
+    }
+
+    async getTreeItem(node: ExplorerNode): Promise<TreeItem> {
+        return node.getTreeItem();
+    }
+
+    async getChildren(node?: ExplorerNode): Promise<ExplorerNode[]> {
+        if (node === undefined) return this._node.getChildren();
+        return node.getChildren();
+    }
+
+    update(uri: GitUri) {
+        this._node = new StashNode(uri, this.context, this.git);
+        this.refresh();
+    }
+
+    refresh() {
+        this._onDidChangeTreeData.fire();
+    }
+}
