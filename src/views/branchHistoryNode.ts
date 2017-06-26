7fbee675f00d403f3347fc40d4864d6427f2c093
Eric Amodio <eamodio@gmail.com>
2017-06-25 23:29:54 -0400

    Spits branch and status nodes into separate files Removes status node for now

---
+++ src/views/branchHistoryNode.ts
@@ -0,0 +1,29 @@
+'use strict';
+import { Iterables } from '../system';
+import { ExtensionContext, TreeItem, TreeItemCollapsibleState } from 'vscode';
+import { CommitNode } from './commitNode';
+import { GlyphChars } from '../constants';
+import { ExplorerNode, ResourceType } from './explorerNode';
+import { GitBranch, GitService, GitUri } from '../gitService';
+
+export class BranchHistoryNode extends ExplorerNode {
+
+        readonly resourceType: ResourceType = 'branch-history';
+
+        constructor(public branch: GitBranch, uri: GitUri, context: ExtensionContext, git: GitService) {
+            super(uri, context, git);
+         }
+
+        async getChildren(): Promise<CommitNode[]> {
+            const log = await this.git.getLogForRepo(this.uri.repoPath!, this.branch.name);
+            if (log === undefined) return [];
+
+            return [...Iterables.map(log.commits.values(), c => new CommitNode(c, this.uri, this.context, this.git))];
+        }
+
+        getTreeItem(): TreeItem {
+            const item = new TreeItem(`${this.branch.name}${this.branch.current ? ` ${GlyphChars.Dash} current` : ''}`, TreeItemCollapsibleState.Collapsed);
+            item.contextValue = this.resourceType;
+            return item;
+        }
+    }
--- src/views/explorerNodes.ts
+++ src/views/explorerNodes.ts
@@ -1,9 +1,12 @@
 'use strict';
 
 export * from './explorerNode';
+export * from './branchesNode';
+export * from './branchHistoryNode';
 export * from './commitFileNode';
 export * from './commitNode';
 export * from './fileHistoryNode';
 export * from './repositoryNode';
 export * from './stashCommitNode';
 export * from './stashNode';
+export * from './statusNode';
