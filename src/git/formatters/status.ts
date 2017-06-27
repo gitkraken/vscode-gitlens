e42af14b11c0fe938382b38bfb2945555a30fa9d
Eric Amodio <eamodio@gmail.com>
2017-06-26 23:54:50 -0400

    Adds status formatter

---
+++ src/git/formatters/status.ts
@@ -0,0 +1,32 @@
+'use strict';
+import { Strings } from '../../system';
+import { Formatter, IFormatOptions } from './formatter';
+import { GitStatusFile, IGitStatusFile } from '../models/status';
+import * as path from 'path';
+
+export interface IStatusFormatOptions extends IFormatOptions {
+    tokenOptions?: {
+        file?: Strings.ITokenOptions;
+        path?: Strings.ITokenOptions;
+    };
+}
+
+export class StatusFileFormatter extends Formatter<IGitStatusFile, IStatusFormatOptions> {
+
+    get file() {
+        const file = path.basename(this._item.fileName);
+        return this._padOrTruncate(file, this._options.tokenOptions!.file);
+    }
+
+    get path() {
+        const directory = GitStatusFile.getFormattedDirectory(this._item, false);
+        return this._padOrTruncate(directory, this._options.tokenOptions!.file);
+    }
+
+    static fromTemplate(template: string, status: IGitStatusFile, dateFormat: string | null): string;
+    static fromTemplate(template: string, status: IGitStatusFile, options?: IStatusFormatOptions): string;
+    static fromTemplate(template: string, status: IGitStatusFile, dateFormatOrOptions?: string | null | IStatusFormatOptions): string;
+    static fromTemplate(template: string, status: IGitStatusFile, dateFormatOrOptions?: string | null | IStatusFormatOptions): string {
+        return super.fromTemplateCore(this, template, status, dateFormatOrOptions);
+    }
+}
