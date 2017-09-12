ea6fdbaaf2a57a9162bce2613d29f834a7c3da6d
Eric Amodio <eamodio@gmail.com>
2017-09-12 15:47:57 -0400

    Adds groupBy function

--- src/system/array.ts
+++ src/system/array.ts
@@ -1,6 +1,15 @@
 'use strict';
 
 export namespace Arrays {
+    export function groupBy<T>(array: T[], accessor: (item: T) => any): T[] {
+        return array.reduce((previous, current) => {
+            const value = accessor(current);
+            previous[value] = previous[value] || [];
+            previous[value].push(current);
+            return previous;
+        }, Object.create(null));
+    }
+
     export function uniqueBy<T>(array: T[], accessor: (item: T) => any, predicate?: (item: T) => boolean): T[] {
         const uniqueValues = Object.create(null);
         return array.filter(_ => {
