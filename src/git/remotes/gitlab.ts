fc3ef4d4f426a2fa6269fef43d80447cd6c48337
Amanda Cameron <amanda@darkdna.net>
2017-09-16 17:33:05 -0400

    Fix GitLab integration's multi-line selection.

--- src/git/remotes/gitlab.ts
+++ src/git/remotes/gitlab.ts
@@ -1,4 +1,5 @@
 'use strict';
+import { Range } from 'vscode';
 import { GitHubService } from './github';
 
 export class GitLabService extends GitHubService {
@@ -9,5 +10,21 @@ export class GitLabService extends GitHubService {
 
     get name() {
         return this.formatName('GitLab');
+    }
+
+    protected getUrlForFile(fileName: string, branch?: string, sha?: string, range?: Range): string {
+        let line = '';
+        if (range) {
+            if (range.start.line === range.end.line) {
+                line = `#L${range.start.line}`;
+            }
+            else {
+                line = `#L${range.start.line}-${range.end.line}`;
+            }
+        }
+
+        if (sha) return `${this.baseUrl}/blob/${sha}/${fileName}${line}`;
+        if (branch) return `${this.baseUrl}/blob/${branch}/${fileName}${line}`;
+        return `${this.baseUrl}?path=${fileName}${line}`;
     }
 }
