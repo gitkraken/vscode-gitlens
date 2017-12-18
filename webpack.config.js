31dd6044e6e3534094e983c5a2e124fd30dc0a28
Eric Amodio <eamodio@gmail.com>
2017-12-18 18:16:21 -0500

    Reduces startup time Adds webpack for bundling (disabled for now) Uses tslib to bundle less duplicate code

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = function(env, argv) {
+    if (env === undefined) {
+        env = {};
+    }
+
+    const minify = !!env.production;
+    const sourceMaps = !env.production;
+
+    const plugins = [
+        new UglifyJsPlugin({
+            parallel: true,
+            sourceMap: sourceMaps,
+            uglifyOptions: {
+                ecma: 7,
+                compress: minify,
+                mangle: minify,
+                output: {
+                    beautify: !minify,
+                    comments: false,
+                    ecma: 7
+                },
+                sourceMap: sourceMaps,
+            }
+        })
+    ];
+
+    return {
+        entry: './src/extension.ts',
+        target: 'node',
+        output: {
+            libraryTarget: 'commonjs2',
+            filename: 'extension.js',
+            path: path.resolve(__dirname, 'out')
+        },
+        resolve: {
+            extensions: ['.ts']
+        },
+        externals: [
+            nodeExternals()
+        ],
+        devtool: sourceMaps ? 'inline-source-map' : false,
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: plugins
+    };
+};
