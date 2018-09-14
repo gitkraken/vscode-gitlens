78aa0a49949abeff111114dda987212f4388fbe2
Eric Amodio <eamodio@gmail.com>
2018-09-14 04:18:00 -0400

    Fixes issue with settings editor not refreshing after being hidden Adds the gitlens icon to settings editor and welcome page

--- src/webviews/webviewEditor.ts
+++ src/webviews/webviewEditor.ts
@@ -49,24 +49,15 @@ export abstract class WebviewEditor<TBootstrap> implements Disposable {
         this._panel = undefined;
     }
 
-    private _invalidateOnVisible: 'all' | 'config' | undefined;
-
     private onViewStateChanged(e: WebviewPanelOnDidChangeViewStateEvent) {
-        Logger.log('WebviewEditor.onViewStateChanged', e.webviewPanel.visible);
-        // HACK: Because messages aren't sent to the webview when hidden, we need make sure it is up-to-date
-        if (this._invalidateOnVisible && e.webviewPanel.visible) {
-            const invalidates = this._invalidateOnVisible;
-            this._invalidateOnVisible = undefined;
+        Logger.log(
+            'WebviewEditor.onViewStateChanged',
+            `active=${e.webviewPanel.active}, visible=${e.webviewPanel.visible}`
+        );
 
-            switch (invalidates) {
-                case 'config':
-                    this.postUpdatedConfiguration();
-                    break;
-
-                default:
-                    void this.show();
-                    break;
-            }
+        // Anytime the webview becomes active, make sure it has the most up-to-date config
+        if (e.webviewPanel.active) {
+            this.postUpdatedConfiguration();
         }
     }
 
@@ -104,21 +95,13 @@ export abstract class WebviewEditor<TBootstrap> implements Disposable {
     }
 
     async show(): Promise<void> {
-        let html = (await this.getHtml()).replace(
-            /{{root}}/g,
-            Uri.file(Container.context.asAbsolutePath('.'))
-                .with({ scheme: 'vscode-resource' })
-                .toString(true)
-        );
-        if (html.includes("'{{bootstrap}}'")) {
-            html = html.replace("'{{bootstrap}}'", JSON.stringify(this.getBootstrap()));
-        }
+        const html = await this.getHtml();
 
         if (this._panel === undefined) {
             this._panel = window.createWebviewPanel(
                 this.id,
                 this.title,
-                ViewColumn.Active, // { viewColumn: ViewColumn.Active, preserveFocus: false }
+                { viewColumn: ViewColumn.Active, preserveFocus: false },
                 {
                     retainContextWhenHidden: true,
                     enableFindWidget: true,
@@ -127,6 +110,7 @@ export abstract class WebviewEditor<TBootstrap> implements Disposable {
                 }
             );
 
+            this._panel.iconPath = Uri.file(Container.context.asAbsolutePath('images/gitlens-icon.png'));
             this._disposablePanel = Disposable.from(
                 this._panel,
                 this._panel.onDidDispose(this.onPanelDisposed, this),
@@ -137,14 +121,19 @@ export abstract class WebviewEditor<TBootstrap> implements Disposable {
             this._panel.webview.html = html;
         }
         else {
+            // Reset the html to get the webview to reload
+            this._panel.webview.html = '';
             this._panel.webview.html = html;
-            this._panel.reveal(ViewColumn.Active); // , false);
+            this._panel.reveal(ViewColumn.Active, false);
         }
     }
 
+    private _html: string | undefined;
     private async getHtml(): Promise<string> {
+        let content;
+        // When we are debugging avoid any caching so that we can change the html and have it update without reloading
         if (Logger.isDebugging) {
-            return new Promise<string>((resolve, reject) => {
+            content = await new Promise<string>((resolve, reject) => {
                 fs.readFile(Container.context.asAbsolutePath(this.filename), 'utf8', (err, data) => {
                     if (err) {
                         reject(err);
@@ -155,29 +144,38 @@ export abstract class WebviewEditor<TBootstrap> implements Disposable {
                 });
             });
         }
+        else {
+            if (this._html !== undefined) return this._html;
 
-        const doc = await workspace.openTextDocument(Container.context.asAbsolutePath(this.filename));
-        return doc.getText();
+            const doc = await workspace.openTextDocument(Container.context.asAbsolutePath(this.filename));
+            content = doc.getText();
+        }
+
+        this._html = content.replace(
+            /{{root}}/g,
+            Uri.file(Container.context.asAbsolutePath('.'))
+                .with({ scheme: 'vscode-resource' })
+                .toString()
+        );
+
+        if (this._html.includes("'{{bootstrap}}'")) {
+            this._html = this._html.replace("'{{bootstrap}}'", JSON.stringify(this.getBootstrap()));
+        }
+
+        return this._html;
     }
 
-    private postMessage(message: Message, invalidates: 'all' | 'config' = 'all') {
+    private postMessage(message: Message) {
         if (this._panel === undefined) return false;
 
-        const result = this._panel!.webview.postMessage(message);
-        if (!result && this._invalidateOnVisible !== 'all') {
-            this._invalidateOnVisible = invalidates;
-        }
-        return result;
+        return this._panel!.webview.postMessage(message);
     }
 
     private postUpdatedConfiguration() {
         // Make sure to get the raw config, not from the container which has the modes mixed in
-        return this.postMessage(
-            {
-                type: 'settingsChanged',
-                config: configuration.get<Config>()
-            } as SettingsChangedMessage,
-            'config'
-        );
+        return this.postMessage({
+            type: 'settingsChanged',
+            config: configuration.get<Config>()
+        } as SettingsChangedMessage);
     }
 }
