ea734c8bcaf150b118b1ff8c9d289b0305302946
Eric Amodio <eamodio@gmail.com>
2019-02-09 12:45:35 -0500

    Fixes webview issues with vscode insiders builds
    See https://github.com/Microsoft/vscode/issues/68211

--- src/ui/shared/app-base.ts
+++ src/ui/shared/app-base.ts
@@ -307,36 +307,50 @@ export abstract class App<TBootstrap extends Bootstrap> {
             const computedStyle = getComputedStyle(body);
 
             const bodyStyle = body.style;
-            let color = computedStyle.getPropertyValue('--color').trim();
-            bodyStyle.setProperty('--color--85', opacity(color, 85));
-            bodyStyle.setProperty('--color--75', opacity(color, 75));
-            bodyStyle.setProperty('--color--50', opacity(color, 50));
 
-            color = computedStyle.getPropertyValue('--background-color').trim();
-            bodyStyle.setProperty('--background-color--lighten-05', lighten(color, 5));
-            bodyStyle.setProperty('--background-color--darken-05', darken(color, 5));
-            bodyStyle.setProperty('--background-color--lighten-075', lighten(color, 7.5));
-            bodyStyle.setProperty('--background-color--darken-075', darken(color, 7.5));
-            bodyStyle.setProperty('--background-color--lighten-15', lighten(color, 15));
-            bodyStyle.setProperty('--background-color--darken-15', darken(color, 15));
-            bodyStyle.setProperty('--background-color--lighten-30', lighten(color, 30));
-            bodyStyle.setProperty('--background-color--darken-30', darken(color, 30));
-
-            color = computedStyle.getPropertyValue('--link-color').trim();
-            bodyStyle.setProperty('--link-color--darken-20', darken(color, 20));
-
+            bodyStyle.setProperty('--font-size', computedStyle.getPropertyValue('--vscode-editor-font-size').trim());
             bodyStyle.setProperty(
-                '--focus-border-color',
-                computedStyle.getPropertyValue('--vscode-focusBorder').trim()
+                '--font-family',
+                computedStyle.getPropertyValue('--vscode-editor-font-family').trim()
             );
+            bodyStyle.setProperty(
+                '--font-weight',
+                computedStyle.getPropertyValue('--vscode-editor-font-weight').trim()
+            );
+
+            let color = computedStyle.getPropertyValue('--vscode-editor-background').trim();
+            bodyStyle.setProperty('--color-background', color);
+            bodyStyle.setProperty('--color-background--lighten-05', lighten(color, 5));
+            bodyStyle.setProperty('--color-background--darken-05', darken(color, 5));
+            bodyStyle.setProperty('--color-background--lighten-075', lighten(color, 7.5));
+            bodyStyle.setProperty('--color-background--darken-075', darken(color, 7.5));
+            bodyStyle.setProperty('--color-background--lighten-15', lighten(color, 15));
+            bodyStyle.setProperty('--color-background--darken-15', darken(color, 15));
+            bodyStyle.setProperty('--color-background--lighten-30', lighten(color, 30));
+            bodyStyle.setProperty('--color-background--darken-30', darken(color, 30));
 
             color = computedStyle.getPropertyValue('--vscode-button-background').trim();
-            bodyStyle.setProperty('--button-background-color', color);
-            bodyStyle.setProperty('--button-background-color--darken-30', darken(color, 30));
-            bodyStyle.setProperty(
-                '--button-color',
-                computedStyle.getPropertyValue('--vscode-button-foreground').trim()
-            );
+            bodyStyle.setProperty('--color-button-background', color);
+            bodyStyle.setProperty('--color-button-background--darken-30', darken(color, 30));
+
+            color = computedStyle.getPropertyValue('--vscode-button-foreground').trim();
+            bodyStyle.setProperty('--color-button-foreground', color);
+
+            color = computedStyle.getPropertyValue('--vscode-editor-foreground').trim();
+            if (!color) {
+                color = computedStyle.getPropertyValue('--vscode-foreground').trim();
+            }
+            bodyStyle.setProperty('--color-foreground', color);
+            bodyStyle.setProperty('--color-foreground--85', opacity(color, 85));
+            bodyStyle.setProperty('--color-foreground--75', opacity(color, 75));
+            bodyStyle.setProperty('--color-foreground--50', opacity(color, 50));
+
+            color = computedStyle.getPropertyValue('--vscode-focusBorder').trim();
+            bodyStyle.setProperty('--color-focus-border', color);
+
+            color = computedStyle.getPropertyValue('--vscode-textLink-foreground').trim();
+            bodyStyle.setProperty('--color-link-foreground', color);
+            bodyStyle.setProperty('--color-link-foreground--darken-20', darken(color, 20));
         };
 
         const observer = new MutationObserver(onColorThemeChanged);
