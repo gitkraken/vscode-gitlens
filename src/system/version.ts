970111615faf3ed93a5712268d9859d43aac0594
Eric Amodio <eamodio@gmail.com>
2019-09-10 23:29:56 -0400

    Adds version string conversion for compare

--- src/system/version.ts
+++ src/system/version.ts
@@ -10,7 +10,14 @@ export namespace Versions {
 		pre?: string;
 	}
 
-	export function compare(v1: Version, v2: Version): VersionComparisonResult {
+	export function compare(v1: string | Version, v2: string | Version): VersionComparisonResult {
+		if (typeof v1 === 'string') {
+			v1 = fromString(v1);
+		}
+		if (typeof v2 === 'string') {
+			v2 = fromString(v2);
+		}
+
 		if (v1.major > v2.major) return 1;
 		if (v1.major < v2.major) return -1;
 
