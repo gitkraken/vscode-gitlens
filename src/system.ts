4e568a851f8bbef14c1f86e3bf7207b0d94559da
Eric Amodio <eamodio@gmail.com>
2019-09-13 02:46:02 -0400

    Fixes Mutable<>, it was as marking props required

--- src/system.ts
+++ src/system.ts
@@ -3,7 +3,7 @@
 export type PartialDeep<T> = T extends object ? { [K in keyof T]?: PartialDeep<T[K]> } : T;
 export type PickPartialDeep<T, K extends keyof T> = Omit<Partial<T>, K> & { [P in K]?: Partial<T[P]> };
 
-export type Mutable<T> = { -readonly [P in keyof T]-?: T[P] };
+export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
 export type PickMutable<T, K extends keyof T> = Omit<T, K> & { -readonly [P in K]: T[P] };
 
 export * from './system/array';
