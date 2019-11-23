c17d3405cc91c0fd8e1a1b2368e5d8c16623133f
Eric Amodio <eamodio@gmail.com>
2019-11-22 23:54:24 -0500

    Fixes #910 - show in search failed from views

--- src/commands/showCommitsInView.ts
+++ src/commands/showCommitsInView.ts
@@ -23,8 +23,6 @@ export class ShowCommitsInViewCommand extends ActiveEditorCommand {
 		args = { ...args };
 
 		if (args.refs === undefined) {
-			if (editor == null) return undefined;
-
 			uri = getCommandUri(uri, editor);
 			if (uri == null) return undefined;
 
@@ -32,23 +30,33 @@ export class ShowCommitsInViewCommand extends ActiveEditorCommand {
 
 			args.repoPath = gitUri.repoPath;
 
-			try {
-				// Check for any uncommitted changes in the range
-				const blame = editor.document.isDirty
-					? await Container.git.getBlameForRangeContents(gitUri, editor.selection, editor.document.getText())
-					: await Container.git.getBlameForRange(gitUri, editor.selection);
-				if (blame === undefined) {
-					return Messages.showFileNotUnderSourceControlWarningMessage(
-						'Unable to show commits in Search Commits view'
-					);
-				}
+			if (editor != null) {
+				try {
+					// Check for any uncommitted changes in the range
+					const blame = editor.document.isDirty
+						? await Container.git.getBlameForRangeContents(
+								gitUri,
+								editor.selection,
+								editor.document.getText()
+						  )
+						: await Container.git.getBlameForRange(gitUri, editor.selection);
+					if (blame === undefined) {
+						return Messages.showFileNotUnderSourceControlWarningMessage(
+							'Unable to show commits in Search Commits view'
+						);
+					}
 
-				args.refs = [
-					...Iterables.filterMap(blame.commits.values(), c => (c.isUncommitted ? undefined : c.ref))
-				];
-			} catch (ex) {
-				Logger.error(ex, 'ShowCommitsInViewCommand', 'getBlameForRange');
-				return Messages.showGenericErrorMessage('Unable to show commits in Search Commits view');
+					args.refs = [
+						...Iterables.filterMap(blame.commits.values(), c => (c.isUncommitted ? undefined : c.ref))
+					];
+				} catch (ex) {
+					Logger.error(ex, 'ShowCommitsInViewCommand', 'getBlameForRange');
+					return Messages.showGenericErrorMessage('Unable to show commits in Search Commits view');
+				}
+			} else {
+				if (gitUri.sha == null) return undefined;
+
+				args.refs = [gitUri.sha];
 			}
 		}
 
