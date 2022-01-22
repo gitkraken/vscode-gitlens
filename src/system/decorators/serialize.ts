efc360d5c115712791b2a22bab67191e371c70ef
Eric Amodio <eamodio@gmail.com>
2022-01-22 01:01:42 -0500

    Improves decorator resolvers

--- src/system/decorators/serialize.ts
+++ src/system/decorators/serialize.ts
@@ -1,21 +1,5 @@
 'use strict';
-import { Uri } from 'vscode';
-
-const emptyStr = '';
-
-function defaultResolver(...args: any[]): string {
-	if (args.length === 1) {
-		const arg0 = args[0];
-		if (arg0 === undefined) return emptyStr;
-		if (typeof arg0 === 'string') return arg0;
-		if (typeof arg0 === 'number' || typeof arg0 === 'boolean' || arg0 instanceof Error) return String(arg0);
-		if (arg0 instanceof Uri) return arg0.toString();
-
-		return JSON.stringify(arg0);
-	}
-
-	return JSON.stringify(args);
-}
+import { resolveProp } from './resolver';
 
 export function serialize<T extends (...arg: any) => any>(
 	resolver?: (...args: Parameters<T>) => string,
@@ -32,11 +16,7 @@ export function serialize<T extends (...arg: any) => any>(
 		const serializeKey = `$serialize$${key}`;
 
 		descriptor.value = function (this: any, ...args: any[]) {
-			const prop =
-				args.length === 0
-					? serializeKey
-					: `${serializeKey}$${(resolver ?? defaultResolver)(...(args as Parameters<T>))}`;
-
+			const prop = resolveProp(serializeKey, resolver, ...(args as Parameters<T>));
 			if (!Object.prototype.hasOwnProperty.call(this, prop)) {
 				Object.defineProperty(this, prop, {
 					configurable: false,
