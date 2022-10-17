aa7ccd988d64a58ffd05392b97bfa29813e17ec6
Eric Amodio <eamodio@gmail.com>
2022-10-17 10:57:02 -0400

    Removes README note from pre-releases Since pre-releases feed the marketplace content

--- scripts/applyPreReleasePatch.js
+++ scripts/applyPreReleasePatch.js
@@ -3,8 +3,10 @@ const fs = require('fs');
 
 // Patch README
 const insert = fs.readFileSync('./README.pre.md', { encoding: 'utf8' });
-const data = fs.readFileSync('./README.md', { encoding: 'utf8' });
-fs.writeFileSync('./README.md', `${insert}\n${data}`);
+if (insert.trim().length !== 0) {
+	const data = fs.readFileSync('./README.md', { encoding: 'utf8' });
+	fs.writeFileSync('./README.md', `${insert}\n${data}`);
+}
 
 // Patch package.json
 const date = new Date(new Date().toLocaleString('en-US', { timeZone: 'America/New_York' }));
