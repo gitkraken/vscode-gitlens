ca861fe52293c240231cea147f8083f2077d06c3
Eric Amodio <eamodio@gmail.com>
2022-11-16 02:04:27 -0500

    Updates dismiss labels

--- src/webviews/apps/home/components/header-card.ts
+++ src/webviews/apps/home/components/header-card.ts
@@ -27,8 +27,8 @@ const template = html<HeaderCard>`
 							class="action is-icon"
 							slot="actions"
 							@click="${(x, c) => x.dismissStatus(c.event as MouseEvent)}"
-							title="Dismiss status update"
-							aria-label="Dismiss status update"
+							title="Dismiss"
+							aria-label="Dismiss"
 							><code-icon icon="close"></code-icon
 						></a>
 					`,
