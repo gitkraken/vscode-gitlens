71b3dd20c9f0f2527b72204621f8f8d2f2d905d6
Keith Daulton <keith.daulton@gitkraken.com>
2023-02-14 03:35:20 -0500

    Updates focus view with PullRequests

--- src/plus/webviews/workspaces/protocol.ts
+++ src/plus/webviews/workspaces/protocol.ts
@@ -1,1 +1,20 @@
-export type State = Record<string, unknown>;
+import type { IssueShape } from '../../../git/models/issue';
+import type { PullRequestShape } from '../../../git/models/pullRequest';
+
+export type State = {
+	pullRequests?: PullRequestResult[];
+	issues?: IssueResult[];
+	[key: string]: unknown;
+};
+
+export interface SearchResultBase {
+	reasons: string[];
+}
+
+export interface IssueResult extends SearchResultBase {
+	issue: IssueShape;
+}
+
+export interface PullRequestResult extends SearchResultBase {
+	pullRequest: PullRequestShape;
+}
--- src/webviews/apps/plus/workspaces/workspaces.ts
+++ src/webviews/apps/plus/workspaces/workspaces.ts
@@ -1,13 +1,13 @@
 import type { State } from '../../../../plus/webviews/workspaces/protocol';
 import { App } from '../../shared/appBase';
+import type { PullRequestRow } from './components/pull-request-row';
 import '../../shared/components/code-icon';
 import '../../shared/components/avatars/avatar-item';
 import '../../shared/components/avatars/avatar-stack';
 import '../../shared/components/table/table-container';
 import '../../shared/components/table/table-row';
 import '../../shared/components/table/table-cell';
-import './components/workspace-list';
-import './components/workspace-item';
+import './components/pull-request-row';
 import './workspaces.scss';
 
 export class WorkspacesApp extends App<State> {
@@ -21,7 +21,23 @@ export class WorkspacesApp extends App<State> {
 		console.log(this.state);
 	}
 
-	renderContent() {}
+	renderContent() {
+		this.renderPullRequests();
+	}
+
+	renderPullRequests() {
+		const prTableEl = document.getElementById('pull-requests');
+
+		if (this.state.pullRequests != null && this.state.pullRequests?.length > 0) {
+			const els = this.state.pullRequests.map(({ pullRequest }) => {
+				const prRowEl = document.createElement('pull-request-row') as PullRequestRow;
+				prRowEl.pullRequest = pullRequest;
+
+				return prRowEl;
+			});
+			prTableEl?.append(...els);
+		}
+	}
 }
 
 new WorkspacesApp();
