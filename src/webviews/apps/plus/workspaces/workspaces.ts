daaf818104fc8fd362a0f98f38288c2099eaf51c
Keith Daulton <keith.daulton@gitkraken.com>
2023-02-14 16:44:14 -0500

    Updates focus view with Issues

--- src/webviews/apps/plus/workspaces/workspaces.ts
+++ src/webviews/apps/plus/workspaces/workspaces.ts
@@ -1,5 +1,6 @@
 import type { State } from '../../../../plus/webviews/workspaces/protocol';
 import { App } from '../../shared/appBase';
+import type { IssueRow } from './components/issue-row';
 import type { PullRequestRow } from './components/pull-request-row';
 import '../../shared/components/code-icon';
 import '../../shared/components/avatars/avatar-item';
@@ -7,6 +8,7 @@ import '../../shared/components/avatars/avatar-stack';
 import '../../shared/components/table/table-container';
 import '../../shared/components/table/table-row';
 import '../../shared/components/table/table-cell';
+import './components/issue-row';
 import './components/pull-request-row';
 import './workspaces.scss';
 
@@ -23,19 +25,36 @@ export class WorkspacesApp extends App<State> {
 
 	renderContent() {
 		this.renderPullRequests();
+		this.renderIssues();
 	}
 
 	renderPullRequests() {
-		const prTableEl = document.getElementById('pull-requests');
+		const tableEl = document.getElementById('pull-requests');
 
 		if (this.state.pullRequests != null && this.state.pullRequests?.length > 0) {
-			const els = this.state.pullRequests.map(({ pullRequest }) => {
-				const prRowEl = document.createElement('pull-request-row') as PullRequestRow;
-				prRowEl.pullRequest = pullRequest;
+			const els = this.state.pullRequests.map(({ pullRequest, reasons }) => {
+				const rowEl = document.createElement('pull-request-row') as PullRequestRow;
+				rowEl.pullRequest = pullRequest;
+				rowEl.reasons = reasons;
 
-				return prRowEl;
+				return rowEl;
 			});
-			prTableEl?.append(...els);
+			tableEl?.append(...els);
+		}
+	}
+
+	renderIssues() {
+		const tableEl = document.getElementById('issues');
+
+		if (this.state.issues != null && this.state.issues?.length > 0) {
+			const els = this.state.issues.map(({ issue, reasons }) => {
+				const rowEl = document.createElement('issue-row') as IssueRow;
+				rowEl.issue = issue;
+				rowEl.reasons = reasons;
+
+				return rowEl;
+			});
+			tableEl?.append(...els);
 		}
 	}
 }
