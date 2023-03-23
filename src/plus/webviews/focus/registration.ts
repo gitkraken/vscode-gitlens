8ac433fdb4803972b63b0839ddab2efecbd8d6ec
Eric Amodio <eamodio@gmail.com>
2023-03-22 23:05:08 -0400

    Adopts new webview model for Focus view

---
+++ src/plus/webviews/focus/registration.ts
@@ -0,0 +1,17 @@
+import { Commands, ContextKeys } from '../../../constants';
+import type { WebviewsController } from '../../../webviews/webviewsController';
+import type { State } from './protocol';
+
+export function registerFocusWebviewPanel(controller: WebviewsController) {
+	return controller.registerWebviewPanel<State>(Commands.ShowFocusPage, 'gitlens.focus', {
+		fileName: 'focus.html',
+		iconPath: 'images/gitlens-icon.png',
+		title: 'Focus View',
+		contextKeyPrefix: `${ContextKeys.WebviewPrefix}focus`,
+		trackingFeature: 'focusWebview',
+		resolveWebviewProvider: async function (container, id, host) {
+			const { FocusWebviewProvider } = await import(/* webpackChunkName: "focus" */ './focusWebview');
+			return new FocusWebviewProvider(container, id, host);
+		},
+	});
+}
