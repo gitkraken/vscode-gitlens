9948e5d85d3bb31b40d1d845499937cac68aeb54
Daniel Rodriguez <sadasant@gmail.com>
2023-04-14 20:35:10 -0400

    Closes #2636 - Adds experimental.OpenAIModel

--- src/config.ts
+++ src/config.ts
@@ -49,6 +49,7 @@ export interface Config {
 	detectNestedRepositories: boolean;
 	experimental: {
 		generateCommitMessagePrompt: string;
+		openAIModel?: 'gpt-3.5-turbo' | 'gpt-3.5-turbo-0301' | 'gpt-4' | 'gpt-4-0314' | 'gpt-4-32k' | 'gpt-4-32k-0314';
 	};
 	fileAnnotations: {
 		command: string | null;
