2687aa0b345456978de4bb113b0fb7ec8afcee05
Eric Amodio <eamodio@gmail.com>
2023-04-30 13:08:42 -0400

    Fixes counter when going back

--- src/commands/git/branch.ts
+++ src/commands/git/branch.ts
@@ -218,7 +218,9 @@ export class BranchGitCommand extends QuickCommand<State> {
 				skippedStepTwo = false;
 				if (context.repos.length === 1) {
 					skippedStepTwo = true;
-					state.counter++;
+					if (state.repo == null) {
+						state.counter++;
+					}
 
 					state.repo = context.repos[0];
 				} else {
--- src/commands/git/worktree.ts
+++ src/commands/git/worktree.ts
@@ -216,7 +216,9 @@ export class WorktreeGitCommand extends QuickCommand<State> {
 				skippedStepTwo = false;
 				if (context.repos.length === 1) {
 					skippedStepTwo = true;
-					state.counter++;
+					if (state.repo == null) {
+						state.counter++;
+					}
 
 					state.repo = context.repos[0];
 				} else {
