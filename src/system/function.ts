bcaad9321fc0da9ca35bc855da82b690e5892ec0
Eric Amodio <eamodio@gmail.com>
2023-05-29 15:28:22 -0400

    Moves throttle and fixes implementation

--- src/system/function.ts
+++ src/system/function.ts
@@ -207,3 +207,27 @@ export async function sequentialize<T extends (...args: any[]) => unknown>(
 export function szudzikPairing(x: number, y: number): number {
 	return x >= y ? x * x + x + y : x + y * y;
 }
+
+export function throttle<T extends (...args: any[]) => ReturnType<T>>(fn: T, delay: number) {
+	let waiting = false;
+	let waitingArgs: Parameters<T> | undefined;
+
+	return function (this: unknown, ...args: Parameters<T>) {
+		if (waiting) {
+			waitingArgs = args;
+
+			return;
+		}
+
+		waiting = true;
+		fn.apply(this, args);
+
+		setTimeout(() => {
+			waiting = false;
+
+			if (waitingArgs != null) {
+				fn.apply(this, waitingArgs);
+			}
+		}, delay);
+	};
+}
