211862ba705f4a56502b168ec1d357629ce8d859
Eric Amodio <eamodio@gmail.com>
2023-06-01 11:01:35 -0400

    Adds reset views location command

--- src/commands/setViewsLayout.ts
+++ src/commands/setViewsLayout.ts
@@ -5,6 +5,19 @@ import type { Container } from '../container';
 import { command, executeCommand, executeCoreCommand } from '../system/command';
 import { Command } from './base';
 
+@command()
+export class ResetViewsLayoutCommand extends Command {
+	constructor(private readonly container: Container) {
+		super(Commands.ResetViewsLayout);
+	}
+
+	async execute() {
+		for (const view of viewsConfigKeys) {
+			void (await executeCommand(`gitlens.views.${view}.resetViewLocation`));
+		}
+	}
+}
+
 export enum ViewsLayout {
 	GitLens = 'gitlens',
 	SourceControl = 'scm',
