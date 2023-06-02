14254ea23e93541b58955a19756814a61a787eb1
Eric Amodio <eamodio@gmail.com>
2023-06-02 01:07:38 -0400

    Adds welcome style header to settings Reorders settings Removes views layout

--- src/webviews/settings/protocol.ts
+++ src/webviews/settings/protocol.ts
@@ -3,6 +3,7 @@ import type { Config } from '../../config';
 export interface State {
 	timestamp: number;
 
+	version: string;
 	config: Config;
 	customSettings?: Record<string, boolean>;
 	scope: 'user' | 'workspace';
--- src/webviews/settings/settingsWebview.ts
+++ src/webviews/settings/settingsWebview.ts
@@ -41,6 +41,7 @@ export class SettingsWebviewProvider extends WebviewProviderWithConfigBase<State
 
 		return {
 			timestamp: Date.now(),
+			version: this.container.version,
 			// Make sure to get the raw config, not from the container which has the modes mixed in
 			config: configuration.getAll(true),
 			customSettings: this.getCustomSettings(),
