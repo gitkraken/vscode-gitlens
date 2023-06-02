102853616f73ef51deba60e8dd048e4696ff4ce8
Keith Daulton <keith.daulton@gitkraken.com>
2023-06-02 11:31:11 -0400

    Adds a button container component

---
+++ src/webviews/apps/shared/components/button-container.ts
@@ -0,0 +1,37 @@
+import { css, html, LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { elementBase } from './styles/lit/base.css';
+
+@customElement('button-container')
+export class ButtonContainer extends LitElement {
+	static override styles = [
+		elementBase,
+		css`
+			:host {
+				display: block;
+				max-width: 30rem;
+				margin-right: auto;
+				margin-left: auto;
+				text-align: left;
+				transition: max-width 0.2s ease-out;
+			}
+
+			@media (min-width: 640px) {
+				:host {
+					max-width: 100%;
+				}
+			}
+
+			.group {
+				display: inline-flex;
+				gap: 0.1rem;
+				width: 100%;
+				max-width: 30rem;
+			}
+		`,
+	];
+
+	override render() {
+		return html`<div class="group"><slot></slot></div>`;
+	}
+}
