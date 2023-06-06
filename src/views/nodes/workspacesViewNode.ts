417cdd038e667659799f0d7da80abe294d99ec3d
Eric Amodio <eamodio@gmail.com>
2023-06-06 03:04:15 -0400

    wip2

--- src/views/nodes/workspacesViewNode.ts
+++ src/views/nodes/workspacesViewNode.ts
@@ -1,3 +1,4 @@
+import { children } from '@microsoft/fast-element';
 import { TreeItem, TreeItemCollapsibleState } from 'vscode';
 import type { WorkspacesView } from '../workspacesView';
 import { MessageNode } from './common';
@@ -20,38 +21,32 @@ export class WorkspacesViewNode extends ViewNode<WorkspacesView> {
 	async getChildren(): Promise<ViewNode[]> {
 		if (this._children == null) {
 			const children: (WorkspaceNode | MessageNode | RepositoriesNode)[] = [];
-			children.push(new RepositoriesNode(this.view));
 
 			const { cloudWorkspaces, cloudWorkspaceInfo, localWorkspaces, localWorkspaceInfo } =
 				await this.view.container.workspaces.getWorkspaces();
-			for (const workspace of cloudWorkspaces) {
-				children.push(new WorkspaceNode(this.uri, this.view, this, workspace));
-			}
 
-			for (const workspace of localWorkspaces) {
-				children.push(new WorkspaceNode(this.uri, this.view, this, workspace));
-			}
+			if (cloudWorkspaces.length || localWorkspaces.length) {
+				children.push(new RepositoriesNode(this.view));
 
-			if (cloudWorkspaceInfo != null) {
-				children.push(
-					new MessageNode(
-						this.view,
-						this,
-						cloudWorkspaceInfo,
-						undefined,
-						undefined,
-						undefined,
-						ContextValues.MessageSignIn,
-					),
-				);
-			}
+				for (const workspace of cloudWorkspaces) {
+					children.push(new WorkspaceNode(this.uri, this.view, this, workspace));
+				}
 
-			if (cloudWorkspaces.length === 0 && cloudWorkspaceInfo == null) {
-				children.push(new MessageNode(this.view, this, 'No cloud workspaces found.'));
-			}
+				if (cloudWorkspaceInfo != null) {
+					children.push(new MessageNode(this.view, this, cloudWorkspaceInfo));
+				}
 
-			if (localWorkspaceInfo != null) {
-				children.push(new MessageNode(this.view, this, localWorkspaceInfo));
+				for (const workspace of localWorkspaces) {
+					children.push(new WorkspaceNode(this.uri, this.view, this, workspace));
+				}
+
+				if (cloudWorkspaces.length === 0 && cloudWorkspaceInfo == null) {
+					children.push(new MessageNode(this.view, this, 'No cloud workspaces found.'));
+				}
+
+				if (localWorkspaceInfo != null) {
+					children.push(new MessageNode(this.view, this, localWorkspaceInfo));
+				}
 			}
 
 			this._children = children;
