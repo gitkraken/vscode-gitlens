e8cfdc6e4c5a7016f848da7c75707decee7a9967
Keith Daulton <keith.daulton@gitkraken.com>
2023-06-08 14:36:17 -0400

    Removes unused code from video-button

--- src/webviews/apps/welcome/components/video-button.ts
+++ src/webviews/apps/welcome/components/video-button.ts
@@ -1,5 +1,5 @@
 import { css, html, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement } from 'lit/decorators.js';
 import { elementBase } from '../../shared/components/styles/lit/base.css';
 
 @customElement('video-button')
