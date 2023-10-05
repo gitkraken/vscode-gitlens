c13db885dfe0be312bf0b34725bac14ddb3f6e1e
Ramin Tadayon <ramin.tadayon@gitkraken.com>
2023-10-05 09:30:29 -0700

    Formatting

--- src/git/models/graph.ts
+++ src/git/models/graph.ts
@@ -1,4 +1,12 @@
-import type { GraphRow, Head, HostingServiceType, Remote, RowContexts, RowStats, Tag } from '@gitkraken/gitkraken-components';
+import type {
+	GraphRow,
+	Head,
+	HostingServiceType,
+	Remote,
+	RowContexts,
+	RowStats,
+	Tag,
+} from '@gitkraken/gitkraken-components';
 import type { GitBranch } from './branch';
 import type { GitRemote } from './remote';
 
