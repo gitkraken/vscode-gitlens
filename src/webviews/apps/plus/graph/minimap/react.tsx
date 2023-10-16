1dbd860f82d92b68b4b51c771754ea897fdb515e
Eric Amodio <eamodio@gmail.com>
2023-10-16 11:48:19 -0400

    Updates dependencies & replaces FAST React w/ Lit

--- src/webviews/apps/plus/graph/minimap/react.tsx
+++ src/webviews/apps/plus/graph/minimap/react.tsx
@@ -1,8 +1,11 @@
+import type { EventName } from '@lit/react';
 import { reactWrapper } from '../../../shared/components/helpers/react-wrapper';
+import type { GraphMinimapDaySelectedEventDetail } from './minimap';
 import { GraphMinimap as graphMinimapComponent } from './minimap';
 
 export const GraphMinimap = reactWrapper(graphMinimapComponent, {
+	tagName: 'graph-minimap',
 	events: {
-		onSelected: 'selected',
+		onSelected: 'selected' as EventName<CustomEvent<GraphMinimapDaySelectedEventDetail>>,
 	},
 });
