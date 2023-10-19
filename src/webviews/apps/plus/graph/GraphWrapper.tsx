74f8ece4565c3729d461c5766d904ceee1bafdb7
Eric Follana <eric.follana@gitkraken.com>
2023-10-19 15:16:13 +0200

    Implemented new property of the GK Graph to format commit messages

--- src/webviews/apps/plus/graph/GraphWrapper.tsx
+++ src/webviews/apps/plus/graph/GraphWrapper.tsx
@@ -17,6 +17,7 @@ import type { FormEvent, ReactElement } from 'react';
 import React, { createElement, useEffect, useMemo, useRef, useState } from 'react';
 import { getPlatform } from '@env/platform';
 import type { DateStyle } from '../../../../config';
+import { emojify } from '../../../../emojis';
 import type { SearchQuery } from '../../../../git/search';
 import type {
 	DidEnsureRowParams,
@@ -162,6 +163,10 @@ const iconElementLibrary = createIconElements();
 
 const getIconElementLibrary = (iconKey: string) => {
 	return iconElementLibrary[iconKey];
+};
+
+const formatCommitMessage = (commitMessage: string) => {
+	return emojify(commitMessage);
 };
 
 const getClientPlatform = (): GraphPlatform => {
@@ -1398,6 +1403,7 @@ export function GraphWrapper({
 							excludeRefsById={excludeRefsById}
 							excludeByType={excludeTypes}
 							formatCommitDateTime={getGraphDateFormatter(graphConfig)}
+							formatCommitMessage={formatCommitMessage}
 							getExternalIcon={getIconElementLibrary}
 							graphRows={rows}
 							hasMoreCommits={pagingHasMore}
