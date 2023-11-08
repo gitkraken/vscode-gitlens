775faa87487825cf5e24aa20372aa819f8378d32
Ramin Tadayon <ramin.tadayon@gitkraken.com>
2023-11-07 17:27:25 -0700

    Adds check for feature flag before prompt

--- src/plus/drafts/actions.ts
+++ src/plus/drafts/actions.ts
@@ -8,18 +8,19 @@ import type { ShowCreateDraft, ShowViewDraft } from '../webviews/patchDetails/re
 type ShowCreateOrOpen = ShowCreateDraft | ShowViewDraft;
 
 export async function showPatchesView(createOrOpen: ShowCreateOrOpen, options?: WebviewViewShowOptions): Promise<void> {
-	const confirm: MessageItem = { title: 'Enable' };
-	const cancel: MessageItem = { title: 'Cancel', isCloseAffordance: true };
-	const result = await window.showInformationMessage(
-		'Cloud Patches are currently disabled. Would you like to enable them?',
-		{ modal: true },
-		confirm,
-		cancel,
-	);
+	if (!configuration.get('cloudPatches.enabled')) {
+		const confirm: MessageItem = { title: 'Enable' };
+		const cancel: MessageItem = { title: 'Cancel', isCloseAffordance: true };
+		const result = await window.showInformationMessage(
+			'Cloud Patches are currently disabled. Would you like to enable them?',
+			{ modal: true },
+			confirm,
+			cancel,
+		);
 
-	if (result !== confirm) return;
-
-	await configuration.updateEffective('cloudPatches.enabled', true);
+		if (result !== confirm) return;
+		await configuration.updateEffective('cloudPatches.enabled', true);
+	}
 
 	if (createOrOpen.mode === 'create') {
 		options = { ...options, preserveFocus: false, preserveVisibility: false };
