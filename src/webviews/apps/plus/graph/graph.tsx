gitkraken/vscode-gitlens

commit be50f2b3ed41fe0c834147fdf86d8b3d5a9852d1

Adds optional binary serialization to ipc messages - Improves performance on sending lots of data to the Graph

src/webviews/apps/plus/graph/graph.tsx
@@ -51,7 +51,9 @@ import {
 	UpdateSelectionCommandType,
 } from '../../../../plus/webviews/graph/protocol';
 import { Color, darken, getCssVariable, lighten, mix, opacity } from '../../../../system/color';
+import { debug } from '../../../../system/decorators/log';
 import { debounce } from '../../../../system/function';
+import { getLogScope, setLogScopeExit } from '../../../../system/logger.scope';
 import type { IpcMessage, IpcNotificationType } from '../../../protocol';
 import { onIpc } from '../../../protocol';
 import { App } from '../../shared/appBase';
@@ -73,7 +75,7 @@ const graphLaneThemeColors = new Map([
 ]);
 
 export class GraphApp extends App<State> {
-	private callback?: UpdateStateCallback;
+	private updateStateCallback?: UpdateStateCallback;
 
 	constructor() {
 		super('GraphApp');
@@ -93,7 +95,7 @@ export class GraphApp extends App<State> {
 				<GraphWrapper
 					nonce={this.state.nonce}
 					state={this.state}
-					subscriber={(callback: UpdateStateCallback) => this.registerEvents(callback)}
+					subscriber={(updateState: UpdateStateCallback) => this.registerUpdateStateCallback(updateState)}
 					onColumnsChange={debounce<GraphApp['onColumnsChanged']>(
 						settings => this.onColumnsChanged(settings),
 						250,
@@ -139,14 +141,13 @@ export class GraphApp extends App<State> {
 	// 	}
 	// }
 
-	protected override onMessageReceived(e: MessageEvent) {
-		const msg = e.data as IpcMessage;
-		this.log(`onMessageReceived(${msg.id}): name=${msg.method}`);
+	protected override onMessageReceived(msg: IpcMessage) {
+		const scope = getLogScope();
 
 		switch (msg.method) {
 			case DidChangeNotificationType.method:
 				onIpc(DidChangeNotificationType, msg, (params, type) => {
-					this.setState({ ...this.state, ...params.state }, type);
+					this.setState({ ...this.state, ...params }, type);
 				});
 				break;
 
@@ -215,7 +216,8 @@ export class GraphApp extends App<State> {
 						const newRowsLength = params.rows.length;
 
 						this.log(
-							`onMessageReceived(${msg.id}:${msg.method}): paging in ${newRowsLength} rows into existing ${previousRowsLength} rows at ${params.paging.startingCursor} (last existing row: ${lastId})`,
+							scope,
+							`paging in ${newRowsLength} rows into existing ${previousRowsLength} rows at ${params.paging.startingCursor} (last existing row: ${lastId})`,
 						);
 
 						rows = [];
@@ -223,18 +225,14 @@ export class GraphApp extends App<State> {
 						rows.length = previousRowsLength + newRowsLength;
 
 						if (params.paging.startingCursor !== lastId) {
-							this.log(
-								`onMessageReceived(${msg.id}:${msg.method}): searching for ${params.paging.startingCursor} in existing rows`,
-							);
+							this.log(scope, `searching for ${params.paging.startingCursor} in existing rows`);
 
 							let i = 0;
 							let row;
 							for (row of previousRows) {
 								rows[i++] = row;
 								if (row.sha === params.paging.startingCursor) {
-									this.log(
-										`onMessageReceived(${msg.id}:${msg.method}): found ${params.paging.startingCursor} in existing rows`,
-									);
+									this.log(scope, `found ${params.paging.startingCursor} in existing rows`);
 
 									previousRowsLength = i;
 
@@ -256,7 +254,7 @@ export class GraphApp extends App<State> {
 							rows[previousRowsLength + i] = params.rows[i];
 						}
 					} else {
-						this.log(`onMessageReceived(${msg.id}:${msg.method}): setting to ${params.rows.length} rows`);
+						this.log(scope, `setting to ${params.rows.length} rows`);
 
 						if (params.rows.length === 0) {
 							rows = this.state.rows;
@@ -281,6 +279,8 @@ export class GraphApp extends App<State> {
 					}
 					this.state.loading = false;
 					this.setState(this.state, type);
+
+					setLogScopeExit(scope, ` \u2022 rows=${this.state.rows?.length ?? 0}`);
 				});
 				break;
 
@@ -339,7 +339,7 @@ export class GraphApp extends App<State> {
 				break;
 
 			default:
-				super.onMessageReceived?.(e);
+				super.onMessageReceived?.(msg);
 		}
 	}
 
@@ -474,14 +474,14 @@ export class GraphApp extends App<State> {
 		this.setState(this.state, 'didChangeTheme');
 	}
 
+	@debug({ args: false, singleLine: true })
 	protected override setState(state: State, type?: IpcNotificationType<any> | InternalNotificationType) {
-		this.log(`setState()`);
 		const themingChanged = this.ensureTheming(state);
 
 		this.state = state;
 		super.setState({ timestamp: state.timestamp, selectedRepository: state.selectedRepository });
 
-		this.callback?.(this.state, type, themingChanged);
+		this.updateStateCallback?.(this.state, type, themingChanged);
 	}
 
 	private ensureTheming(state: State): boolean {
@@ -691,11 +691,11 @@ export class GraphApp extends App<State> {
 		});
 	}
 
-	private registerEvents(callback: UpdateStateCallback): () => void {
-		this.callback = callback;
+	private registerUpdateStateCallback(updateState: UpdateStateCallback): () => void {
+		this.updateStateCallback = updateState;
 
 		return () => {
-			this.callback = undefined;
+			this.updateStateCallback = undefined;
 		};
 	}
 }

src/plus/webviews/graph/protocol.ts
@@ -309,7 +309,7 @@ export const UpdateSelectionCommandType = new IpcCommandType<UpdateSelectionPara
 export interface DidChangeParams {
 	state: State;
 }
-export const DidChangeNotificationType = new IpcNotificationType<DidChangeParams>('graph/didChange', true);
+export const DidChangeNotificationType = new IpcNotificationType<DidChangeParams>('graph/didChange', true, true);
 
 export interface DidChangeGraphConfigurationParams {
 	config: GraphComponentConfig;
@@ -387,7 +387,11 @@ export interface DidChangeRowsParams {
 	rowsStatsLoading: boolean;
 	selectedRows?: GraphSelectedRows;
 }
-export const DidChangeRowsNotificationType = new IpcNotificationType<DidChangeRowsParams>('graph/rows/didChange');
+export const DidChangeRowsNotificationType = new IpcNotificationType<DidChangeRowsParams>(
+	'graph/rows/didChange',
+	undefined,
+	true,
+);
 
 export interface DidChangeRowsStatsParams {
 	rowsStats: Record<string, GraphRowStats>;

src/system/logger.scope.ts
@@ -18,7 +18,9 @@ export function getLogScope(): LogScope | undefined {
 	return scopes.get(scopeCounter);
 }
 
-export function getNewLogScope(prefix: string): LogScope {
+export function getNewLogScope(prefix: string, scope?: LogScope | undefined): LogScope {
+	if (scope != null) return { scopeId: scope.scopeId, prefix: `${scope.prefix}${prefix}` };
+
 	const scopeId = getNextLogScopeId();
 	return {
 		scopeId: scopeId,

src/webviews/apps/commitDetails/commitDetails.ts
@@ -105,10 +105,7 @@ export class CommitDetailsApp extends App<Serialized<State>> {
 		return disposables;
 	}
 
-	protected override onMessageReceived(e: MessageEvent) {
-		const msg = e.data as IpcMessage;
-		this.log(`onMessageReceived(${msg.id}): name=${msg.method}`);
-
+	protected override onMessageReceived(msg: IpcMessage) {
 		switch (msg.method) {
 			// case DidChangeRichStateNotificationType.method:
 			// 	onIpc(DidChangeRichStateNotificationType, msg, params => {
@@ -152,7 +149,7 @@ export class CommitDetailsApp extends App<Serialized<State>> {
 				break;
 
 			default:
-				super.onMessageReceived?.(e);
+				super.onMessageReceived?.(msg);
 		}
 	}
 

src/webviews/apps/home/home.ts
@@ -38,13 +38,9 @@ export class HomeApp extends App<State> {
 		return disposables;
 	}
 
-	protected override onMessageReceived(e: MessageEvent) {
-		const msg = e.data as IpcMessage;
-
+	protected override onMessageReceived(msg: IpcMessage) {
 		switch (msg.method) {
 			case DidChangeRepositoriesType.method:
-				this.log(`onMessageReceived(${msg.id}): name=${msg.method}`);
-
 				onIpc(DidChangeRepositoriesType, msg, params => {
 					this.state.repositories = params;
 					this.state.timestamp = Date.now();
@@ -53,7 +49,7 @@ export class HomeApp extends App<State> {
 				});
 				break;
 			default:
-				super.onMessageReceived?.(e);
+				super.onMessageReceived?.(msg);
 				break;
 		}
 	}

src/webviews/apps/plus/account/account.ts
@@ -31,13 +31,9 @@ export class AccountApp extends App<State> {
 		return disposables;
 	}
 
-	protected override onMessageReceived(e: MessageEvent) {
-		const msg = e.data as IpcMessage;
-
+	protected override onMessageReceived(msg: IpcMessage) {
 		switch (msg.method) {
 			case DidChangeSubscriptionNotificationType.method:
-				this.log(`onMessageReceived(${msg.id}): name=${msg.method}`);
-
 				onIpc(DidChangeSubscriptionNotificationType, msg, params => {
 					this.state.subscription = params.subscription;
 					this.state.avatar = params.avatar;
@@ -48,7 +44,7 @@ export class AccountApp extends App<State> {
 				break;
 
 			default:
-				super.onMessageReceived?.(e);
+				super.onMessageReceived?.(msg);
 				break;
 		}
 	}

src/webviews/apps/plus/focus/focus.ts
@@ -118,10 +118,7 @@ export class FocusApp extends App<State> {
 		}
 	}
 
-	protected override onMessageReceived(e: MessageEvent) {
-		const msg = e.data as IpcMessage;
-		this.log(`onMessageReceived(${msg.id}): name=${msg.method}`);
-
+	protected override onMessageReceived(msg: IpcMessage) {
 		switch (msg.method) {
 			case DidChangeNotificationType.method:
 				onIpc(DidChangeNotificationType, msg, params => {
@@ -130,6 +127,8 @@ export class FocusApp extends App<State> {
 					this.attachState();
 				});
 				break;
+			default:
+				super.onMessageReceived?.(msg);
 		}
 	}
 }

src/webviews/apps/plus/patchDetails/patchDetails.ts
@@ -135,10 +135,7 @@ export class PatchDetailsApp extends App<Serialized<State>> {
 		return disposables;
 	}
 
-	protected override onMessageReceived(e: MessageEvent) {
-		const msg = e.data as IpcMessage;
-		this.log(`onMessageReceived(${msg.id}): name=${msg.method}`);
-
+	protected override onMessageReceived(msg: IpcMessage) {
 		switch (msg.method) {
 			// case DidChangeRichStateNotificationType.method:
 			// 	onIpc(DidChangeRichStateNotificationType, msg, params => {
@@ -222,7 +219,7 @@ export class PatchDetailsApp extends App<Serialized<State>> {
 				break;
 
 			default:
-				super.onMessageReceived?.(e);
+				super.onMessageReceived?.(msg);
 		}
 	}
 

commit 74b958b74e4de31c578a154ff1f1d9b11ea89885

Fixes regression caused by be50f2b

src/webviews/apps/plus/graph/graph.tsx
@@ -147,7 +147,7 @@ export class GraphApp extends App<State> {
 		switch (msg.method) {
 			case DidChangeNotificationType.method:
 				onIpc(DidChangeNotificationType, msg, (params, type) => {
-					this.setState({ ...this.state, ...params }, type);
+					this.setState({ ...this.state, ...params.state }, type);
 				});
 				break;
 