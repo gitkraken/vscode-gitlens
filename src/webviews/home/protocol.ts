b9bb2a3dce68968e5ec2018538c5235417084669
Keith Daulton <keith.daulton@gitkraken.com>
2023-11-30 12:43:46 -0500

    Adds cyber week banner to home

--- src/webviews/home/protocol.ts
+++ src/webviews/home/protocol.ts
@@ -4,6 +4,7 @@ import { IpcNotificationType } from '../protocol';
 export interface State extends WebviewState {
 	repositories: DidChangeRepositoriesParams;
 	webroot?: string;
+	promoStates: Record<string, boolean>;
 }
 
 export interface DidChangeRepositoriesParams {
@@ -13,3 +14,8 @@ export interface DidChangeRepositoriesParams {
 	trusted: boolean;
 }
 export const DidChangeRepositoriesType = new IpcNotificationType<DidChangeRepositoriesParams>('repositories/didChange');
+
+export interface DidChangeSubscriptionParams {
+	promoStates: Record<string, boolean>;
+}
+export const DidChangeSubscriptionType = new IpcNotificationType<DidChangeSubscriptionParams>('subscription/didChange');
