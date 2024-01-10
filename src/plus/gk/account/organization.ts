c0f724aa1a8771d23690d4e22dbfbbbaa53a57c1
Keith Daulton <keith.daulton@gitkraken.com>
2024-01-09 20:50:05 -0500

    Adds service call to get Organization members
    Updates OrganizationConnection type

--- src/plus/gk/account/organization.ts
+++ src/plus/gk/account/organization.ts
@@ -7,3 +7,33 @@ export interface Organization {
 export type OrganizationRole = 'owner' | 'admin' | 'billing' | 'user';
 
 export type OrganizationsResponse = Organization[];
+
+export interface FullOrganization {
+	readonly id: string;
+	readonly name: string;
+	readonly domain: string;
+	readonly updatedToNewRoles: boolean;
+	readonly memberCount: number;
+	readonly members: OrganizationMember[];
+	readonly connections: OrganizationConnection[];
+	readonly type: OrganizationType;
+	readonly isOnChargebee: boolean;
+}
+
+export enum OrganizationType {
+	Enterprise = 'ENTERPRISE',
+	Individual = 'INDIVIDUAL',
+	Pro = 'PRO',
+	Teams = 'TEAMS',
+}
+
+export type OrganizationConnection = Record<string, unknown>;
+
+export interface OrganizationMember {
+	readonly id: string;
+	readonly email: string;
+	readonly name: string;
+	readonly username: string;
+	readonly role: OrganizationRole;
+	readonly licenseConsumption: Record<string, boolean>;
+}
