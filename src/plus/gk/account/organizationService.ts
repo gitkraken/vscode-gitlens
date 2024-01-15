dc223cf20c9e5b5c0452ac78d1e331800a65e3f8
Keith Daulton <keith.daulton@gitkraken.com>
2024-01-15 18:07:14 -0500

    Removes option chain

--- src/plus/gk/account/organizationService.ts
+++ src/plus/gk/account/organizationService.ts
@@ -148,7 +148,7 @@ export class OrganizationService implements Disposable {
 			if (this._fullOrganizations == null) {
 				this._fullOrganizations = new Map();
 			}
-			this._fullOrganizations?.set(id, organization);
+			this._fullOrganizations.set(id, organization);
 		}
 		return this._fullOrganizations.get(id);
 	}
