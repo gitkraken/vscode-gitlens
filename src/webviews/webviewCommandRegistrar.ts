41dd5f3155130177e76bd7f279bfe33acf9b1f2e
Eric Amodio <eamodio@gmail.com>
2024-04-03 01:27:37 -0400

    Renames for clarity

--- src/webviews/webviewCommandRegistrar.ts
+++ src/webviews/webviewCommandRegistrar.ts
@@ -30,20 +30,22 @@ export class WebviewCommandRegistrar implements Disposable {
 				subscription: registerWebviewCommand(
 					command,
 					(...args: any[]) => {
-						const item = args[0];
-						if (!isWebviewContext(item)) {
+						const [context] = args;
+						if (!isWebviewContext(context)) {
 							debugger;
 							return;
 						}
 
-						const key = item.webviewInstance ? `${item.webview}:${item.webviewInstance}` : item.webview;
+						const key = context.webviewInstance
+							? `${context.webview}:${context.webviewInstance}`
+							: context.webview;
 
 						const handler = handlers.get(key);
 						if (handler == null) {
 							throw new Error(`Unable to find Command '${command}' registration for Webview '${key}'`);
 						}
 
-						handler.callback.call(handler.thisArg, item);
+						handler.callback.call(handler.thisArg, context);
 					},
 					this,
 				),
