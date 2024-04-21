10ddda328d12017f9f3a8d497b69114dce5f418e
Eric Amodio <eamodio@gmail.com>
2024-04-21 17:36:19 -0400

    Removes `token` as it is already provided

--- src/plus/gk/account/organizationService.ts
+++ src/plus/gk/account/organizationService.ts
@@ -148,16 +148,7 @@ export class OrganizationService implements Disposable {
 		options?: { force?: boolean },
 	): Promise<FullOrganization | undefined> {
 		if (!this._fullOrganizations?.has(id) || options?.force === true) {
-			const session = await this.container.subscription.getAuthenticationSession();
-
-			const rsp = await this.connection.fetchApi(
-				`organization/${id}`,
-				{
-					method: 'GET',
-				},
-				{ token: session?.accessToken },
-			);
-
+			const rsp = await this.connection.fetchApi(`organization/${id}`, { method: 'GET' });
 			if (!rsp.ok) {
 				Logger.error(
 					'',
@@ -206,16 +197,7 @@ export class OrganizationService implements Disposable {
 		if (id == null) return undefined;
 
 		if (!this._organizationSettings?.has(id) || options?.force === true) {
-			const session = await this.container.subscription.getAuthenticationSession();
-
-			const rsp = await this.connection.fetchApi(
-				`v1/organizations/settings`,
-				{
-					method: 'GET',
-				},
-				{ token: session?.accessToken },
-			);
-
+			const rsp = await this.connection.fetchApi(`v1/organizations/settings`, { method: 'GET' });
 			if (!rsp.ok) {
 				Logger.error(
 					'',
