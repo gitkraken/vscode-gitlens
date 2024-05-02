28509a4399993c9aa1570e3cbe372e170c0b9287
Keith Daulton <keith.daulton@gitkraken.com>
2024-05-02 17:03:27 -0400

    Adds indicators to repo status tab WIP

--- src/webviews/apps/shared/components/pills/pill.css.ts
+++ src/webviews/apps/shared/components/pills/pill.css.ts
@@ -2,11 +2,13 @@ import { css } from 'lit';
 
 export const baseStyles = css`
 	:host {
+		box-sizing: border-box;
 		display: inline-block;
 		vertical-align: text-bottom;
 	}
 
 	.pill {
+		box-sizing: border-box;
 		display: inline-flex;
 		align-items: center;
 		justify-content: center;
@@ -14,9 +16,15 @@ export const baseStyles = css`
 		border-radius: 0.5rem;
 		font-size: 1rem;
 		font-weight: 500;
-		line-height: 1;
+		line-height: normal;
 		text-transform: uppercase;
-		color: var(--vscode-foreground);
-		background-color: var(--vscode-editorWidget-background);
+		color: var(--gl-pill-foreground, var(--vscode-foreground));
+		background-color: var(--gl-pill-background, var(--vscode-editorWidget-background));
+	}
+
+	.pill--outlined {
+		padding: 0.1rem 0.4rem;
+		background-color: transparent;
+		border: 1px solid var(--gl-pill-border, var(--vscode-foreground));
 	}
 `;
