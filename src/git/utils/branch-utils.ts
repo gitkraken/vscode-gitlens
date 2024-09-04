637543b9df70c1e5e1e3357d8cdf12b5e7adf9fc
Eric Amodio <eamodio@gmail.com>
2024-09-04 12:45:13 -0400

    Removes unused function

--- src/git/utils/branch-utils.ts
+++ src/git/utils/branch-utils.ts
@@ -4,20 +4,6 @@ import type { Container } from '../../container';
 import { getIconPathUris } from '../../system/vscode';
 import type { GitBranch } from '../models/branch';
 
-export type BranchIconStatus = 'ahead' | 'behind' | 'diverged' | 'synced';
-export function getBranchIconStatus(branch: GitBranch): BranchIconStatus | undefined {
-	switch (branch.status) {
-		case 'ahead':
-		case 'behind':
-		case 'diverged':
-			return branch.status;
-		case 'upToDate':
-			return 'synced';
-		default:
-			return undefined;
-	}
-}
-
 export function getBranchIconPath(container: Container, branch: GitBranch | undefined): IconPath {
 	const status = branch?.status;
 	switch (status) {
