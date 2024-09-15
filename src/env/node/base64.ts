efdbfb219daf1113b3ce866638185d16b7a3adc3
Eric Amodio <eamodio@gmail.com>
2024-09-15 18:03:41 -0400

    Updates dependencies

--- src/env/node/base64.ts
+++ src/env/node/base64.ts
@@ -5,5 +5,5 @@ export function base64(data: string | Uint8Array): string {
 }
 
 export function fromBase64(s: string): Uint8Array {
-	return Buffer.from(s, 'base64');
+	return Buffer.from(s, 'base64') as unknown as Uint8Array;
 }
