9f4cd3a2b079083f7b5e18e18c3498060c59b928
Keith Daulton <keith.daulton@gitkraken.com>
2024-09-25 18:02:54 -0400

    Remove pop-menu component

--- src/webviews/apps/shared/components/overlays/pop-menu/react.tsx
+++
@@ -1,4 +0,0 @@
-import { reactWrapper } from '../../helpers/react-wrapper';
-import { PopMenu as PopMenuComponent } from './index';
-
-export const PopMenu = reactWrapper(PopMenuComponent, { tagName: 'pop-menu' });
