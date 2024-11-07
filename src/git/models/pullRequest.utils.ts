f104e5b89be6b2354d76325e683114a94dcb687b
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2024-11-07 14:52:44 +0100

    Fixes unit tests by uncoupling dependencies: It has been broken by adding 'slug' dependency that pulled to the test assembly through Container, so we need to uncouple. (#3621, #3698)

---
+++ src/git/models/pullRequest.utils.ts
@@ -0,0 +1,42 @@
+// pullRequest.ts pulls many dependencies through Container and some of them break the unit tests.
+// To avoid this file has been created that can collect more simple functions which
+// don't require Container and can be tested.
+
+export type PullRequestURLIdentity = {
+	ownerAndRepo?: string;
+	prNumber?: string;
+};
+
+export function getPullRequestIdentityValuesFromSearch(search: string): PullRequestURLIdentity {
+	let ownerAndRepo: string | undefined = undefined;
+	let prNumber: string | undefined = undefined;
+
+	let match = search.match(/([^/]+\/[^/]+)\/pull\/(\d+)/); // with org and rep name
+	if (match != null) {
+		ownerAndRepo = match[1];
+		prNumber = match[2];
+	}
+
+	if (prNumber == null) {
+		match = search.match(/(?:\/|^)pull\/(\d+)/); // without repo name
+		if (match != null) {
+			prNumber = match[1];
+		}
+	}
+
+	if (prNumber == null) {
+		match = search.match(/(?:\/)(\d+)/); // any number starting with "/"
+		if (match != null) {
+			prNumber = match[1];
+		}
+	}
+
+	if (prNumber == null) {
+		match = search.match(/^#?(\d+)$/); // just a number or with a leading "#"
+		if (match != null) {
+			prNumber = match[1];
+		}
+	}
+
+	return { ownerAndRepo: ownerAndRepo, prNumber: prNumber };
+}
