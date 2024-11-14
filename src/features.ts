d11fa9985fc7fc7efd4640059ff8d2f4ddc92a6e
Sergio <86774052+sergiolms@users.noreply.github.com>
2024-11-14 16:24:33 +0100

    fix: Feature Preview compare hours elapsed within range instead of dates (#3752)
    * compare hours elapsed within range instead of dates
    
    * replace magic number

--- src/features.ts
+++ src/features.ts
@@ -1,5 +1,5 @@
 import type { StoredFeaturePreviewUsagePeriod } from './constants.storage';
-import { proFeaturePreviewUsages } from './constants.subscription';
+import { proFeaturePreviewUsageDurationInDays, proFeaturePreviewUsages } from './constants.subscription';
 import type { RepositoryVisibility } from './git/gitProvider';
 import type { RequiredSubscriptionPlans, Subscription } from './plus/gk/account/subscription';
 import { capitalize } from './system/string';
@@ -60,17 +60,28 @@ export function getFeaturePreviewLabel(feature: FeaturePreviews) {
 	}
 }
 
+const hoursInMs = 3600000;
+
 export function isFeaturePreviewActive(featurePreview?: FeaturePreview) {
 	const usages = featurePreview?.usages;
 	if (usages == null || usages.length === 0) return false;
 
-	return usages.length <= proFeaturePreviewUsages && new Date(usages[usages.length - 1].expiresOn) > new Date();
+	const remainingHours = (new Date(usages[usages.length - 1].expiresOn).getTime() - new Date().getTime()) / hoursInMs;
+	return (
+		usages.length <= proFeaturePreviewUsages &&
+		remainingHours > 0 &&
+		remainingHours < 24 * proFeaturePreviewUsageDurationInDays
+	);
 }
 
 export function isFeaturePreviewExpired(featurePreview: FeaturePreview) {
 	const usages = featurePreview.usages;
+	if (usages == null || usages.length === 0) return false;
+
+	const remainingHours = (new Date(usages[usages.length - 1].expiresOn).getTime() - new Date().getTime()) / hoursInMs;
 	return (
 		usages.length > proFeaturePreviewUsages ||
-		(usages.length === proFeaturePreviewUsages && new Date(usages[usages.length - 1].expiresOn) < new Date())
+		(usages.length === proFeaturePreviewUsages && remainingHours <= 0) ||
+		remainingHours >= 24 * proFeaturePreviewUsageDurationInDays
 	);
 }
