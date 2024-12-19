30faba68483332619d022f8c7206303fa5396d51
Brunojoe1995 <137604361+Brunojoe1995@users.noreply.github.com>
2024-12-19 15:05:10 +0300

    setup.ts

--- tests/e2e/setup.ts
+++ tests/e2e/setup.ts
@@ -1,7 +1,1 @@
-import { downloadAndUnzipVSCode } from '@vscode/test-electron';
 
-// eslint-disable-next-line import-x/no-default-export
-export default async () => {
-	await downloadAndUnzipVSCode('insiders');
-	await downloadAndUnzipVSCode('stable');
-};
