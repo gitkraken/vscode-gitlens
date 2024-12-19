23e531a0c004018d8bdae31b39c59fbca8d01c49
Eric Amodio <eamodio@gmail.com>
2024-12-19 03:11:02 -0500

    Adds tooltips and indicator coloring

---
+++ src/webviews/apps/shared/components/branch-name.ts
@@ -0,0 +1,41 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import './code-icon';
+
+@customElement('gl-branch-name')
+export class GlBranchName extends LitElement {
+	static override styles = css`
+		:host {
+			display: inline-flex;
+			align-items: center;
+			white-space: nowrap;
+		}
+
+		strong {
+			font-weight: bold;
+		}
+	`;
+
+	@property({ type: String })
+	name?: string;
+
+	@property({ type: Number })
+	size: number = 12;
+
+	override render() {
+		return html`
+			<code-icon icon="git-branch" size="${this.size}"></code-icon>&nbsp;<strong
+				>${this.name ?? '<missing>'}</strong
+			>
+		`;
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'gl-branch-name': GlBranchName;
+	}
+}
+export function renderBranchName(name: string | undefined, size = 12) {
+	return html`<gl-branch-name .name=${name} .size=${size}></gl-branch-name>`;
+}
--- src/webviews/apps/shared/components/card/card.css.ts
+++ src/webviews/apps/shared/components/card/card.css.ts
@@ -1,6 +1,13 @@
 import { css } from 'lit';
 
 export const cardStyles = css`
+	:host {
+		--gl-color-mix-base: var(
+			--gl-card-background,
+			color-mix(in lab, var(--vscode-sideBar-background) 100%, #fff 5%)
+		);
+	}
+
 	.card {
 		display: block;
 		flex-direction: column;
@@ -8,6 +15,7 @@ export const cardStyles = css`
 		padding: 0.8rem 1.2rem;
 		border-radius: 0.4rem;
 		border-inline-start: 0.3rem solid transparent;
+		border-inline-end: 0.3rem solid transparent;
 		background-color: var(--gl-card-background, color-mix(in lab, var(--vscode-sideBar-background) 100%, #fff 5%));
 		margin-block-end: 0.6rem;
 		position: relative;
@@ -29,6 +37,90 @@ export const cardStyles = css`
 		border-inline-start-color: var(
 			--gl-card-indicator-border,
 			var(--vscode-gitlens-decorations\\.statusMergingOrRebasingConflictForegroundColor)
+		);
+	}
+
+	.card.is-issue-open {
+		border-inline-end-color: var(--gl-card-indicator-border, var(--vscode-gitlens-openAutolinkedIssueIconColor));
+	}
+
+	.card.is-issue-closed {
+		border-inline-end-color: var(--gl-card-indicator-border, var(--vscode-gitlens-closedAutolinkedIssueIconColor));
+	}
+
+	.card.is-pr-open {
+		border-inline-end-color: var(--gl-card-indicator-border, var(--vscode-gitlens-openPullRequestIconColor));
+	}
+
+	.card.is-pr-closed {
+		border-inline-end-color: var(--gl-card-indicator-border, var(--vscode-gitlens-closedPullRequestIconColor));
+	}
+
+	.card.is-pr-merged {
+		border-inline-end-color: var(--gl-card-indicator-border, var(--vscode-gitlens-mergedPullRequestIconColor));
+	}
+
+	.card.is-mergeable {
+		border-inline-end-color: var(
+			--gl-card-indicator-border,
+			var(var(--vscode-gitlens-launchpadIndicatorMergeableColor))
+		);
+	}
+
+	.card.is-blocked {
+		border-inline-end-color: var(--gl-card-indicator-border, var(--vscode-gitlens-launchpadIndicatorBlockedColor));
+	}
+
+	.card.is-attention {
+		border-inline-end-color: var(
+			--gl-card-indicator-border,
+			var(--vscode-gitlens-launchpadIndicatorAttentionColor)
+		);
+	}
+
+	.card.is-branch-synced {
+		border-inline-start-color: var(
+			--gl-card-indicator-border,
+			color-mix(in lab, var(--gl-color-mix-base) 100%, #fff 20%)
+		);
+	}
+
+	.card.is-branch-diverged {
+		border-inline-start-color: var(
+			--gl-card-indicator-border,
+			color-mix(
+				in lab,
+				var(--vscode-gitlens-decorations\\.branchDivergedForegroundColor) 100%,
+				var(--gl-color-mix-base) 50%
+			)
+		);
+	}
+
+	.card.is-branch-behind {
+		border-inline-start-color: var(
+			--gl-card-indicator-border,
+			var(--vscode-gitlens-decorations\\.branchBehindForegroundColor)
+		);
+	}
+
+	.card.is-branch-ahead {
+		border-inline-start-color: var(
+			--gl-card-indicator-border,
+			var(--vscode-gitlens-decorations\\.branchBehindForegroundColor)
+		);
+	}
+
+	.card.is-branch-changes {
+		border-inline-start-color: var(
+			--gl-card-indicator-border,
+			color-mix(in lab, #1a79ff 100%, var(--gl-color-mix-base) 10%)
+		);
+	}
+
+	.card.is-branch-missingUpstream {
+		border-inline-start-color: var(
+			--gl-card-indicator-border,
+			var(--vscode-gitlens-decorations\\.branchMissingUpstreamForegroundColor)
 		);
 	}
 
