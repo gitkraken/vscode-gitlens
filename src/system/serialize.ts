35b8a4ef4b7ecc95fd93611e551bb8501d2cdd14
Eric Amodio <eamodio@gmail.com>
2025-02-06 18:57:36 -0500

    Improves type accuracy

--- src/system/serialize.ts
+++ src/system/serialize.ts
@@ -1,23 +1,28 @@
 import type { Uri } from 'vscode';
 import { serializeJsonReplacer } from '@env/json';
+import type { Container } from '../container';
 import type { Branded } from './brand';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export type Serialized<T> = T extends Function
+// prettier-ignore
+export type Serialized<T> =
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+	T extends Function | Error | Container
 	? never
 	: T extends Date
-	  ? number
-	  : T extends Uri
-	    ? string
-	    : T extends Branded<infer U, any>
-	      ? U
-	      : T extends any[]
-	        ? Serialized<T[number]>[]
-	        : T extends object
-	          ? {
-								[K in keyof T]: T[K] extends Date ? number : Serialized<T[K]>;
-	            }
-	          : T;
+	? number
+	: T extends Uri | RegExp
+	? string
+	: T extends Map<infer K, infer V>
+	? [Serialized<K>, Serialized<V>][]
+	: T extends Set<infer U>
+	? Serialized<U>[]
+	: T extends Branded<infer U, any>
+	? U
+	: T extends any[]
+	? Serialized<T[number]>[]
+	: T extends object
+	? { [K in keyof T]: T[K] extends Date ? number : Serialized<T[K]> }
+	: T;
 
 export function serialize<T extends object>(obj: T): Serialized<T>;
 export function serialize<T extends object>(obj: T | undefined): Serialized<T> | undefined;
