d0b1b796ff816110ba04993d68ccc8505cbe25a6
Eric Amodio <eamodio@gmail.com>
2025-02-10 12:25:11 -0500

    Adds promos context to AppBase

--- src/webviews/apps/shared/appBase.ts
+++ src/webviews/apps/shared/appBase.ts
@@ -14,6 +14,7 @@ import type {
 import { DidChangeWebviewFocusNotification, WebviewFocusChangedCommand, WebviewReadyCommand } from '../../protocol';
 import { ipcContext } from './contexts/ipc';
 import { loggerContext, LoggerContext } from './contexts/logger';
+import { PromosContext, promosContext } from './contexts/promos';
 import { telemetryContext, TelemetryContext } from './contexts/telemetry';
 import { DOM } from './dom';
 import type { Disposable } from './events';
@@ -32,6 +33,7 @@ export abstract class App<
 	private readonly _api: HostIpcApi;
 	private readonly _hostIpc: HostIpc;
 	private readonly _logger: LoggerContext;
+	private readonly _promos: PromosContext;
 	protected readonly _telemetry: TelemetryContext;
 
 	protected state: State;
@@ -58,6 +60,9 @@ export abstract class App<
 		this._hostIpc = new HostIpc(this.appName);
 		disposables.push(this._hostIpc);
 
+		this._promos = new PromosContext(this._hostIpc);
+		disposables.push(this._promos);
+
 		this._telemetry = new TelemetryContext(this._hostIpc);
 		disposables.push(this._telemetry);
 
@@ -65,6 +70,10 @@ export abstract class App<
 		new ContextProvider(document.body, {
 			context: loggerContext,
 			initialValue: this._logger,
+		});
+		new ContextProvider(document.body, {
+			context: promosContext,
+			initialValue: this._promos,
 		});
 		new ContextProvider(document.body, {
 			context: telemetryContext,
