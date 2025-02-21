0631a9424a20722188fb4e4dcdcb6c8a607842c6
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2025-02-21 12:46:33 +0100

    Stops sending request in draftsService with empty entityIds
    (#4099)

--- src/plus/drafts/draftsService.ts
+++ src/plus/drafts/draftsService.ts
@@ -856,6 +856,10 @@ export class DraftService implements Disposable {
 				return EntityIdentifierUtils.encode(getEntityIdentifierInput(pr));
 			});
 
+		if (prEntityIds.length === 0) {
+			return {};
+		}
+
 		const body = JSON.stringify({
 			prEntityIds: prEntityIds,
 		});
