gitkraken/vscode-gitlens

commit 7a7da0f45b01a149c0c4adb25af858e813fa9493

Refactors Git provider into sub-providers  - Revison operations Removes unneeded Git abstractions

src/env/node/git/sub-providers/staging.ts
@@ -39,7 +39,7 @@ export class StagingGitSubProvider implements GitStagingSubProvider {
 			// Create the temp index file from a base ref/sha
 
 			// Get the tree of the base
-			const newIndex = await this.git.exec<string>(
+			const newIndex = await this.git.exec(
 				{
 					cwd: repoPath,
 					env: env,
@@ -52,7 +52,7 @@ export class StagingGitSubProvider implements GitStagingSubProvider {
 			);
 
 			// Write the tree to our temp index
-			await this.git.exec<string>(
+			await this.git.exec(
 				{
 					cwd: repoPath,
 					env: env,
@@ -81,11 +81,9 @@ export class StagingGitSubProvider implements GitStagingSubProvider {
 
 	@log()
 	async stageFile(repoPath: string, pathOrUri: string | Uri, options?: { intentToAdd?: boolean }): Promise<void> {
-		await this.git.add(
-			repoPath,
-			[typeof pathOrUri === 'string' ? pathOrUri : splitPath(pathOrUri, repoPath)[0]],
-			options?.intentToAdd ? '-N' : '-A',
-		);
+		await this.git.exec({ cwd: repoPath }, 'add', options?.intentToAdd ? '-N' : '-A', '--', [
+			typeof pathOrUri === 'string' ? pathOrUri : splitPath(pathOrUri, repoPath)[0],
+		]);
 	}
 
 	@log()
@@ -94,10 +92,12 @@ export class StagingGitSubProvider implements GitStagingSubProvider {
 		pathOrUri: string[] | Uri[],
 		options?: { intentToAdd?: boolean },
 	): Promise<void> {
-		await this.git.add(
-			repoPath,
-			pathOrUri.map(p => (typeof p === 'string' ? p : splitPath(p, repoPath)[0])),
+		await this.git.exec(
+			{ cwd: repoPath },
+			'add',
 			options?.intentToAdd ? '-N' : '-A',
+			'--',
+			pathOrUri.map(p => (typeof p === 'string' ? p : splitPath(p, repoPath)[0])),
 		);
 	}
 
@@ -107,11 +107,9 @@ export class StagingGitSubProvider implements GitStagingSubProvider {
 		directoryOrUri: string | Uri,
 		options?: { intentToAdd?: boolean },
 	): Promise<void> {
-		await this.git.add(
-			repoPath,
-			[typeof directoryOrUri === 'string' ? directoryOrUri : splitPath(directoryOrUri, repoPath)[0]],
-			options?.intentToAdd ? '-N' : '-A',
-		);
+		await this.git.exec({ cwd: repoPath }, 'add', options?.intentToAdd ? '-N' : '-A', '--', [
+			typeof directoryOrUri === 'string' ? directoryOrUri : splitPath(directoryOrUri, repoPath)[0],
+		]);
 	}
 
 	@log()

src/env/node/git/localGitProvider.ts
@@ -11,6 +11,7 @@ import { GlyphChars, Schemes } from '../../../constants';
 import type { Container } from '../../../container';
 import { Features } from '../../../features';
 import { GitCache } from '../../../git/cache';
+import { GitErrorHandling } from '../../../git/commandOptions';
 import {
 	BlameIgnoreRevsFileBadRevisionError,
 	BlameIgnoreRevsFileError,
@@ -42,11 +43,9 @@ import { RemoteResourceType } from '../../../git/models/remoteResource';
 import type { RepositoryChangeEvent } from '../../../git/models/repository';
 import { Repository, RepositoryChange, RepositoryChangeComparisonMode } from '../../../git/models/repository';
 import { deletedOrMissing } from '../../../git/models/revision';
-import type { GitTreeEntry } from '../../../git/models/tree';
 import { parseGitBlame } from '../../../git/parsers/blameParser';
 import { parseGitFileDiff } from '../../../git/parsers/diffParser';
 import { parseGitLogSimpleFormat, parseGitLogSimpleRenamed } from '../../../git/parsers/logParser';
-import { parseGitLsFiles, parseGitTree } from '../../../git/parsers/treeParser';
 import { getBranchNameAndRemote, getBranchTrackingWithoutRemote } from '../../../git/utils/branch.utils';
 import { isBranchReference } from '../../../git/utils/reference.utils';
 import { getVisibilityCacheKey } from '../../../git/utils/remote.utils';
@@ -65,7 +64,7 @@ import { getBestPath, isFolderUri, relative, splitPath } from '../../../system/-
 import { gate } from '../../../system/decorators/-webview/gate';
 import { debug, log } from '../../../system/decorators/log';
 import { debounce } from '../../../system/function';
-import { first } from '../../../system/iterable';
+import { first, join } from '../../../system/iterable';
 import { Logger } from '../../../system/logger';
 import type { LogScope } from '../../../system/logger.scope';
 import { getLogScope, setLogScopeExit } from '../../../system/logger.scope';
@@ -89,6 +88,7 @@ import { GraphGitSubProvider } from './sub-providers/graph';
 import { PatchGitSubProvider } from './sub-providers/patch';
 import { RefsGitSubProvider } from './sub-providers/refs';
 import { RemotesGitSubProvider } from './sub-providers/remotes';
+import { RevisionGitSubProvider } from './sub-providers/revision';
 import { StagingGitSubProvider } from './sub-providers/staging';
 import { StashGitSubProvider } from './sub-providers/stash';
 import { StatusGitSubProvider } from './sub-providers/status';
@@ -935,7 +935,7 @@ export class LocalGitProvider implements GitProvider, Disposable {
 		let patch;
 		try {
 			patch = await this.git.diff(root, relativePath, ref1, ref2);
-			void (await this.git.apply(root, patch));
+			void (await this.git.exec({ cwd: root, stdin: patch }, 'apply', '--whitespace=warn'));
 		} catch (ex) {
 			const msg: string = ex?.toString() ?? '';
 			if (patch && /patch does not apply/i.test(msg)) {
@@ -949,7 +949,7 @@ export class LocalGitProvider implements GitProvider, Disposable {
 
 				if (result.title === 'Yes') {
 					try {
-						void (await this.git.apply(root, patch, { allowConflicts: true }));
+						void (await this.git.exec({ cwd: root, stdin: patch }, 'apply', '--whitespace=warn', '--3way'));
 
 						return;
 					} catch (e) {
@@ -1007,7 +1007,12 @@ export class LocalGitProvider implements GitProvider, Disposable {
 	async excludeIgnoredUris(repoPath: string, uris: Uri[]): Promise<Uri[]> {
 		const paths = new Map<string, Uri>(uris.map(u => [normalizePath(u.fsPath), u]));
 
-		const data = await this.git.check_ignore(repoPath, ...paths.keys());
+		const data = await this.git.exec(
+			{ cwd: repoPath, errors: GitErrorHandling.Ignore, stdin: join(paths.keys(), '\0') },
+			'check-ignore',
+			'-z',
+			'--stdin',
+		);
 		if (data == null) return uris;
 
 		const ignored = data.split('\0').filter(<T>(i?: T): i is T => Boolean(i));
@@ -1134,11 +1139,12 @@ export class LocalGitProvider implements GitProvider, Disposable {
 
 			// Since Git can't setup remote tracking when publishing a new branch to a specific commit, do it now
 			if (setUpstream != null) {
-				await this.git.branch__set_upstream(
-					repoPath,
+				await this.git.exec(
+					{ cwd: repoPath },
+					'branch',
+					'--set-upstream-to',
+					`${setUpstream.remote}/${setUpstream.remoteBranch}`,
 					setUpstream.branch,
-					setUpstream.remote,
-					setUpstream.remoteBranch,
 				);
 			}
 
@@ -1898,49 +1904,6 @@ export class LocalGitProvider implements GitProvider, Disposable {
 		return undefined;
 	}
 
-	@gate()
-	@log()
-	getRevisionContent(repoPath: string, path: string, ref: string): Promise<Uint8Array | undefined> {
-		const [relativePath, root] = splitPath(path, repoPath);
-
-		return this.git.show__content<Buffer>(root, relativePath, ref, { encoding: 'buffer' }) as Promise<
-			Uint8Array | undefined
-		>;
-	}
-
-	@log()
-	async getTreeEntryForRevision(repoPath: string, path: string, ref: string): Promise<GitTreeEntry | undefined> {
-		if (repoPath == null || !path) return undefined;
-
-		const [relativePath, root] = splitPath(path, repoPath);
-
-		if (isUncommittedStaged(ref)) {
-			const data = await this.git.ls_files(root, relativePath, { ref: ref });
-			const [result] = parseGitLsFiles(data);
-			if (result == null) return undefined;
-
-			const size = await this.git.cat_file__size(repoPath, result.oid);
-			return {
-				ref: ref,
-				oid: result.oid,
-				path: relativePath,
-				size: size,
-				type: 'blob',
-			};
-		}
-
-		const data = await this.git.ls_tree(root, ref, relativePath);
-		return parseGitTree(data, ref)[0];
-	}
-
-	@log()
-	async getTreeForRevision(repoPath: string, ref: string): Promise<GitTreeEntry[]> {
-		if (repoPath == null) return [];
-
-		const data = await this.git.ls_tree(repoPath, ref);
-		return parseGitTree(data, ref);
-	}
-
 	hasUnsafeRepositories(): boolean {
 		return this.unsafePaths.size !== 0;
 	}
@@ -2049,10 +2012,10 @@ export class LocalGitProvider implements GitProvider, Disposable {
 				}
 
 				if (!tracked && ref && !isUncommitted(ref)) {
-					tracked = Boolean(await this.git.ls_files(repoPath, relativePath, { ref: ref }));
+					tracked = Boolean(await this.git.ls_files(repoPath, relativePath, { rev: ref }));
 					// If we still haven't found this file, make sure it wasn't deleted in that ref (i.e. check the previous)
 					if (!tracked) {
-						tracked = Boolean(await this.git.ls_files(repoPath, relativePath, { ref: `${ref}^` }));
+						tracked = Boolean(await this.git.ls_files(repoPath, relativePath, { rev: `${ref}^` }));
 					}
 				}
 
@@ -2146,6 +2109,11 @@ export class LocalGitProvider implements GitProvider, Disposable {
 	private _remotes: RemotesGitSubProvider | undefined;
 	get remotes(): RemotesGitSubProvider {
 		return (this._remotes ??= new RemotesGitSubProvider(this.container, this.git, this._cache, this));
+	}
+
+	private _revision: RevisionGitSubProvider | undefined;
+	get revision(): RevisionGitSubProvider {
+		return (this._revision ??= new RevisionGitSubProvider(this.container, this.git, this._cache, this));
 	}
 
 	private _staging: StagingGitSubProvider | undefined;

src/env/node/git/sub-providers/branches.ts
@@ -12,7 +12,7 @@ import type {
 import { GitBranch } from '../../../../git/models/branch';
 import type { MergeConflict } from '../../../../git/models/mergeConflict';
 import type { GitBranchReference } from '../../../../git/models/reference';
-import { parseGitBranches } from '../../../../git/parsers/branchParser';
+import { parseGitBranches, parseGitBranchesDefaultFormat } from '../../../../git/parsers/branchParser';
 import { parseMergeTreeConflict } from '../../../../git/parsers/mergeTreeParser';
 import { getReferenceFromBranch } from '../../../../git/utils/-webview/reference.utils';
 import type { BranchSortOptions } from '../../../../git/utils/-webview/sorting';
@@ -28,7 +28,9 @@ import { getLogScope } from '../../../../system/logger.scope';
 import { PageableResult } from '../../../../system/paging';
 import { getSettledValue } from '../../../../system/promise';
 import type { Git } from '../git';
+import { GitErrors, gitLogDefaultConfigs } from '../git';
 import type { LocalGitProvider } from '../localGitProvider';
+import { RunError } from '../shell';
 
 const emptyPagedResult: PagedResult<any> = Object.freeze({ values: [] });
 
@@ -76,7 +78,16 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 
 		const [pausedOpStatusResult, committerDateResult] = await Promise.allSettled([
 			isDetachedHead(name) ? this.provider.status?.getPausedOperationStatus(repoPath) : undefined,
-			this.git.log__recent_committerdate(repoPath, commitOrdering),
+			this.git
+				.exec(
+					{ cwd: repoPath, configs: gitLogDefaultConfigs, errors: GitErrorHandling.Ignore },
+					'log',
+					'-n1',
+					'--format=%ct',
+					commitOrdering ? `--${commitOrdering}-order` : undefined,
+					'--',
+				)
+				.then(data => (!data.length ? undefined : data.trim())),
 		]);
 
 		const committerDate = getSettledValue(committerDateResult);
@@ -114,7 +125,13 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 		if (resultsPromise == null) {
 			async function load(this: BranchesGitSubProvider): Promise<PagedResult<GitBranch>> {
 				try {
-					const data = await this.git.for_each_ref__branch(repoPath, { all: true });
+					const data = await this.git.exec(
+						{ cwd: repoPath },
+						'for-each-ref',
+						`--format=${parseGitBranchesDefaultFormat}`,
+						'refs/heads',
+						'refs/remotes',
+					);
 					// If we don't get any data, assume the repo doesn't have any commits yet so check if we have a current branch
 					if (!data?.length) {
 						const current = await this.getCurrentBranch(repoPath);
@@ -275,7 +292,7 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 
 		if (remote) {
 			try {
-				const data = await this.git.ls_remote__HEAD(repoPath, remote);
+				const data = await this.git.exec({ cwd: repoPath }, 'ls-remote', '--symref', remote, 'HEAD');
 				if (data == null) return undefined;
 
 				const match = /ref:\s(\S+)\s+HEAD/m.exec(data);
@@ -287,7 +304,7 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 		}
 
 		try {
-			const data = await this.git.symbolic_ref(repoPath, `refs/remotes/origin/HEAD`);
+			const data = await this.git.exec({ cwd: repoPath }, 'symbolic-ref', '--short', 'refs/remotes/origin/HEAD');
 			return data?.trim() || undefined;
 		} catch {}
 
@@ -304,8 +321,14 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 		const scope = getLogScope();
 
 		try {
-			const data = await this.git.merge_base(repoPath, ref1, ref2, options);
-			if (data == null) return undefined;
+			const data = await this.git.exec(
+				{ cwd: repoPath },
+				'merge-base',
+				options?.forkPoint ? '--fork-point' : undefined,
+				ref1,
+				ref2,
+			);
+			if (!data) return undefined;
 
 			return data.split('\n')[0].trim() || undefined;
 		} catch (ex) {
@@ -316,7 +339,7 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 
 	@log()
 	async createBranch(repoPath: string, name: string, sha: string): Promise<void> {
-		await this.git.branch(repoPath, name, sha);
+		await this.git.exec({ cwd: repoPath }, 'branch', name, sha);
 	}
 
 	@log()
@@ -371,14 +394,7 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 			} catch {}
 
 			// Cherry-pick detection (handles cherry-picks, rebases, etc)
-			const data = await this.git.exec<string>(
-				{ cwd: repoPath },
-				'cherry',
-				'--abbrev',
-				'-v',
-				into.name,
-				branch.name,
-			);
+			const data = await this.git.exec({ cwd: repoPath }, 'cherry', '--abbrev', '-v', into.name, branch.name);
 			// Check if there are no lines or all lines startwith a `-` (i.e. likely merged)
 			if (!data || data.split('\n').every(l => l.startsWith('-'))) {
 				return { merged: true, confidence: 'high' };
@@ -415,9 +431,37 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 
 			let data;
 			try {
-				data = await this.git.merge_tree(repoPath, branch, targetBranch, '-z', '--name-only', '--no-messages');
+				data = await this.git.exec(
+					{ cwd: repoPath, errors: GitErrorHandling.Throw },
+					'merge-tree',
+					'-z',
+					'--name-only',
+					'--no-messages',
+					branch,
+					targetBranch,
+				);
 			} catch (ex) {
-				Logger.error(ex, scope);
+				const msg: string = ex?.toString() ?? '';
+				if (GitErrors.notAValidObjectName.test(msg)) {
+					Logger.error(
+						ex,
+						scope,
+						`'${targetBranch}' or '${branch}' not found - ensure the branches exist and are fully qualified (e.g. 'refs/heads/main')`,
+					);
+				} else if (GitErrors.badRevision.test(msg)) {
+					Logger.error(ex, scope, `Invalid branch name: ${msg.slice(msg.indexOf("'"))}`);
+				} else if (GitErrors.noMergeBase.test(msg)) {
+					Logger.error(
+						ex,
+						scope,
+						`Unable to merge '${branch}' and '${targetBranch}' as they have no common ancestor`,
+					);
+				} else if (ex instanceof RunError) {
+					data = ex.stdout;
+				} else {
+					Logger.error(ex, scope);
+					debugger;
+				}
 			}
 			if (!data) return undefined;
 
@@ -496,7 +540,7 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 		options?: { upstream: true },
 	): Promise<string | undefined> {
 		try {
-			let data = await this.git.reflog(repoPath, undefined, ref, '--grep-reflog=branch: Created from *.');
+			let data = await this.git.exec({ cwd: repoPath }, 'reflog', ref, '--grep-reflog=branch: Created from *.');
 
 			let entries = data.split('\n').filter(entry => Boolean(entry));
 			if (entries.length !== 1) return undefined;
@@ -517,12 +561,13 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 			}
 
 			// Check if branch was created from HEAD
-			data = await this.git.reflog(
-				repoPath,
-				undefined,
+			data = await this.git.exec(
+				{ cwd: repoPath },
+				'reflog',
 				'HEAD',
 				`--grep-reflog=checkout: moving from .* to ${ref.replace('refs/heads/', '')}`,
 			);
+
 			entries = data.split('\n').filter(entry => Boolean(entry));
 			if (!entries.length) return undefined;
 
@@ -562,11 +607,11 @@ export class BranchesGitSubProvider implements GitBranchesSubProvider {
 
 	@log()
 	async renameBranch(repoPath: string, oldName: string, newName: string): Promise<void> {
-		await this.git.branch(repoPath, '-m', oldName, newName);
+		await this.git.exec({ cwd: repoPath }, 'branch', '-m', oldName, newName);
 	}
 
 	private async getValidatedBranchName(repoPath: string, name: string): Promise<string | undefined> {
-		const data = await this.git.exec<string>(
+		const data = await this.git.exec(
 			{ cwd: repoPath, errors: GitErrorHandling.Ignore },
 			'rev-parse',
 			'--verify',

src/env/node/git/sub-providers/commits.ts
@@ -3,6 +3,7 @@ import { Range } from 'vscode';
 import type { SearchQuery } from '../../../../constants.search';
 import type { Container } from '../../../../container';
 import type { GitCache } from '../../../../git/cache';
+import { GitErrorHandling } from '../../../../git/commandOptions';
 import type { GitCommitsSubProvider, LeftRightCommitCountResult } from '../../../../git/gitProvider';
 import { GitUri } from '../../../../git/gitUri';
 import type { GitBlame } from '../../../../git/models/blame';
@@ -40,7 +41,7 @@ import { isFolderGlob } from '../../../../system/path';
 import type { CachedLog, TrackedGitDocument } from '../../../../trackers/trackedDocument';
 import { GitDocumentState } from '../../../../trackers/trackedDocument';
 import type { Git } from '../git';
-import { gitLogDefaultConfigsWithFiles } from '../git';
+import { gitLogDefaultConfigs, gitLogDefaultConfigsWithFiles } from '../git';
 import type { LocalGitProvider } from '../localGitProvider';
 
 const emptyPromise: Promise<GitBlame | GitDiffFile | GitLog | undefined> = Promise.resolve(undefined);
@@ -77,8 +78,14 @@ export class CommitsGitSubProvider implements GitCommitsSubProvider {
 	}
 
 	@log({ exit: true })
-	getCommitCount(repoPath: string, rev: string): Promise<number | undefined> {
-		return this.git.rev_list__count(repoPath, rev);
+	async getCommitCount(repoPath: string, rev: string): Promise<number | undefined> {
+		const data = (
+			await this.git.exec({ cwd: repoPath, errors: GitErrorHandling.Ignore }, 'rev-list', '--count', rev, '--')
+		)?.trim();
+		if (!data?.length) return undefined;
+
+		const result = parseInt(data, 10);
+		return isNaN(result) ? undefined : result;
 	}
 
 	@log()
@@ -120,7 +127,15 @@ export class CommitsGitSubProvider implements GitCommitsSubProvider {
 		const [relativePath, root] = splitPath(uri, repoPath);
 
 		// Don't include the filename, as renames won't be returned
-		const data = await this.git.show(root, undefined, '--name-status', '--format=', '-z', rev, '--');
+		const data = await this.git.exec(
+			{ cwd: root, configs: gitLogDefaultConfigs },
+			'show',
+			'--name-status',
+			'--format=',
+			'-z',
+			rev,
+			'--',
+		);
 		if (!data) return undefined;
 
 		const files = parseGitDiffNameStatusFiles(data, repoPath);
@@ -253,20 +268,51 @@ export class CommitsGitSubProvider implements GitCommitsSubProvider {
 	@log({ exit: true })
 	async getInitialCommitSha(repoPath: string): Promise<string | undefined> {
 		try {
-			const data = await this.git.rev_list(repoPath, 'HEAD', { maxParents: 0 });
-			return data?.[0];
+			const data = await this.git.exec(
+				{ cwd: repoPath, errors: GitErrorHandling.Ignore },
+				'rev-list',
+				`--max-parents=0`,
+				'HEAD',
+				'--',
+			);
+			return data?.trim().split('\n')?.[0];
 		} catch {
 			return undefined;
 		}
 	}
 
 	@log()
-	getLeftRightCommitCount(
+	async getLeftRightCommitCount(
 		repoPath: string,
 		range: GitRevisionRange,
 		options?: { authors?: GitUser[] | undefined; excludeMerges?: boolean },
 	): Promise<LeftRightCommitCountResult | undefined> {
-		return this.git.rev_list__left_right(repoPath, range, options?.authors, options?.excludeMerges);
+		const authors = options?.authors?.length ? options.authors.map(a => `--author=^${a.name} <${a.email}>$`) : [];
+
+		const data = await this.git.exec(
+			{ cwd: repoPath, errors: GitErrorHandling.Ignore },
+			'rev-list',
+			'--left-right',
+			'--count',
+			...authors,
+			options?.excludeMerges ? '--no-merges' : undefined,
+			range,
+			'--',
+		);
+		if (!data?.length) return undefined;
+
+		const parts = data.split('\t');
+		if (parts.length !== 2) return undefined;
+
+		const [left, right] = parts;
+		const result = {
+			left: parseInt(left, 10),
+			right: parseInt(right, 10),
+		};
+
+		if (isNaN(result.left) || isNaN(result.right)) return undefined;
+
+		return result;
 	}
 
 	@log()
@@ -949,14 +995,21 @@ export class CommitsGitSubProvider implements GitCommitsSubProvider {
 	async hasCommitBeenPushed(repoPath: string, rev: string): Promise<boolean> {
 		if (repoPath == null) return false;
 
-		return this.git.merge_base__is_ancestor(repoPath, rev, '@{u}');
+		return this.isAncestorOf(repoPath, rev, '@{u}');
 	}
 
 	@log()
 	async isAncestorOf(repoPath: string, rev1: string, rev2: string): Promise<boolean> {
 		if (repoPath == null) return false;
 
-		return this.git.merge_base__is_ancestor(repoPath, rev1, rev2);
+		const exitCode = await this.git.exec(
+			{ cwd: repoPath, exitCodeOnly: true },
+			'merge-base',
+			'--is-ancestor',
+			rev1,
+			rev2,
+		);
+		return exitCode === 0;
 	}
 
 	@log<CommitsGitSubProvider['searchCommits']>({
@@ -980,14 +1033,8 @@ export class CommitsGitSubProvider implements GitCommitsSubProvider {
 
 			const currentUser = await this.provider.config.getCurrentUser(repoPath);
 
-			const { args, files, shas } = getGitArgsFromSearchQuery(search, currentUser);
-
-			args.push(`-M${similarityThreshold == null ? '' : `${similarityThreshold}%`}`, '--');
-			if (files.length !== 0) {
-				args.push(...files);
-			}
-
-			const includeOnlyStashes = args.includes('--no-walk');
+			const { args: searchArgs, files, shas } = getGitArgsFromSearchQuery(search, currentUser);
+			const includeOnlyStashes = searchArgs.includes('--no-walk');
 
 			let stashes: Map<string, GitStashCommit> | undefined;
 			let stdin: string | undefined;
@@ -1009,13 +1056,34 @@ export class CommitsGitSubProvider implements GitCommitsSubProvider {
 				}
 			}
 
-			const data = await this.git.log__search(repoPath, shas?.size ? undefined : args, {
-				ordering: configuration.get('advanced.commitOrdering'),
-				...options,
-				limit: limit,
-				shas: shas,
-				stdin: stdin,
-			});
+			let data;
+			if (!shas?.size) {
+				data = await this.git.exec(
+					{ cwd: repoPath, configs: ['-C', repoPath, ...gitLogDefaultConfigs], stdin: stdin },
+					'log',
+					stdin ? '--stdin' : undefined,
+					'--name-status',
+					`--format=${parseGitLogDefaultFormat}`,
+					'--use-mailmap',
+					`-M${similarityThreshold == null ? '' : `${similarityThreshold}%`}`,
+					...searchArgs,
+					options?.ordering ? `--${options.ordering}-order` : undefined,
+					options?.limit ? `-n${options.limit + 1}` : undefined,
+					options?.skip ? `--skip=${options.skip}` : undefined,
+					'--',
+					...files,
+				);
+			} else {
+				data = await this.git.exec(
+					{ cwd: repoPath, stdin: join(shas, '\n') },
+					'show',
+					'--stdin',
+					'--name-status',
+					`--format=${parseGitLogDefaultFormat}`,
+					'--use-mailmap',
+				);
+			}
+
 			const log = parseGitLog(
 				this.container,
 				data,

src/env/node/git/sub-providers/config.ts
@@ -4,6 +4,7 @@ import { Uri } from 'vscode';
 import type { GitConfigKeys } from '../../../../constants';
 import type { Container } from '../../../../container';
 import type { GitCache } from '../../../../git/cache';
+import { GitErrorHandling } from '../../../../git/commandOptions';
 import type { GitConfigSubProvider, GitDir } from '../../../../git/gitProvider';
 import type { GitUser } from '../../../../git/models/user';
 import { gate } from '../../../../system/decorators/-webview/gate';
@@ -28,8 +29,13 @@ export class ConfigGitSubProvider implements GitConfigSubProvider {
 		return this.git.config__get(key, repoPath);
 	}
 
-	setConfig(repoPath: string, key: GitConfigKeys, value: string | undefined): Promise<void> {
-		return this.git.config__set(key, value, repoPath);
+	async setConfig(repoPath: string, key: GitConfigKeys, value: string | undefined): Promise<void> {
+		await this.git.exec(
+			{ cwd: repoPath ?? '', local: true },
+			'config',
+			'--local',
+			...(value == null ? ['--unset', key] : [key, value]),
+		);
 	}
 
 	@gate()
@@ -81,7 +87,12 @@ export class ConfigGitSubProvider implements GitConfigSubProvider {
 
 			const author = `${user.name} <${user.email}>`;
 			// Check if there is a mailmap for the current user
-			const mappedAuthor = await this.git.check_mailmap(repoPath, author);
+			const mappedAuthor = await this.git.exec(
+				{ cwd: repoPath, errors: GitErrorHandling.Ignore },
+				'check-mailmap',
+				author,
+			);
+
 			if (mappedAuthor != null && mappedAuthor.length !== 0 && author !== mappedAuthor) {
 				const match = mappedAuthorRegex.exec(mappedAuthor);
 				if (match != null) {

src/env/node/git/sub-providers/contributors.ts
@@ -149,17 +149,16 @@ export class ContributorsGitSubProvider implements GitContributorsSubProvider {
 
 		const scope = getLogScope();
 
-		const args = ['shortlog', '-s', '--all'];
-		if (!options?.merges) {
-			args.push('--no-merges');
-		}
-		if (options?.since) {
-			args.push(`--since=${options.since}`);
-		}
-
 		try {
-			const data = await this.git.exec<string>({ cwd: repoPath }, ...args);
-			if (data == null) return undefined;
+			const data = await this.git.exec(
+				{ cwd: repoPath },
+				'shortlog',
+				'-s',
+				'--all',
+				!options?.merges ? '--no-merges' : undefined,
+				options?.since ? `--since=${options.since}` : undefined,
+			);
+			if (!data) return undefined;
 
 			const contributions = data
 				.split('\n')

commit b6e28b2774fec4fb1b91332f5d91c4a6e6260030

Fixes staging multiple files Caused by 7a7da0f45b01a149c0c4adb25af858e813fa9493

src/env/node/git/sub-providers/staging.ts
@@ -97,7 +97,7 @@ export class StagingGitSubProvider implements GitStagingSubProvider {
 			'add',
 			options?.intentToAdd ? '-N' : '-A',
 			'--',
-			pathOrUri.map(p => (typeof p === 'string' ? p : splitPath(p, repoPath)[0])),
+			...pathOrUri.map(p => (typeof p === 'string' ? p : splitPath(p, repoPath)[0])),
 		);
 	}
 