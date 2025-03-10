e96961448d4247ee3aaa23b085d8f0fe162f45dc
Ramin Tadayon <ramin.tadayon@gitkraken.com>
2025-03-10 15:22:04 -0700

    Updates Start Work and Associate Issue flows

--- src/commands/quickCommand.steps.ts
+++ src/commands/quickCommand.steps.ts
@@ -2705,6 +2705,28 @@ export async function* ensureAccessStep<
 				createQuickPickSeparator(),
 			);
 			break;
+		case PlusFeatures.StartWork:
+			directives.splice(
+				0,
+				0,
+				createDirectiveQuickPickItem(Directive.Noop, undefined, {
+					label: 'Start work on an issue from your connected integrations',
+					iconPath: new ThemeIcon('issues'),
+				}),
+				createQuickPickSeparator(),
+			);
+			break;
+		case PlusFeatures.AssociateIssueWithBranch:
+			directives.splice(
+				0,
+				0,
+				createDirectiveQuickPickItem(Directive.Noop, undefined, {
+					label: 'Connect your branches to their associated issues in Home view',
+					iconPath: new ThemeIcon('issues'),
+				}),
+				createQuickPickSeparator(),
+			);
+			break;
 		case PlusFeatures.Worktrees:
 			directives.splice(
 				0,
