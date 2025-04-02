e72c557b28837e9ba98b63dad062d12cae6100c0
Ramin Tadayon <ramin.tadayon@gitkraken.com>
2025-04-02 08:13:03 -0700

    Only passes repo for open in worktree Launchpad action
    Closes #4196

--- src/plus/launchpad/launchpadProvider.ts
+++ src/plus/launchpad/launchpadProvider.ts
@@ -492,9 +492,11 @@ export class LaunchpadProvider implements Disposable {
 		);
 		if (deepLinkUrl == null) return;
 
-		const prRepo = await getOrOpenPullRequestRepository(this.container, item.underlyingPullRequest, {
-			skipVirtual: true,
-		});
+		const prRepo = options?.openInWorktree
+			? await getOrOpenPullRequestRepository(this.container, item.underlyingPullRequest, {
+					skipVirtual: true,
+			  })
+			: undefined;
 		this._codeSuggestions?.delete(item.uuid);
 		await this.container.deepLinks.processDeepLinkUri(deepLinkUrl, false, prRepo);
 	}
