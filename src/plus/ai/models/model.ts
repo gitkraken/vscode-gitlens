ada53fc9617486f39a7bcc0c3b515d7b907c5247
Eric Amodio <eamodio@gmail.com>
2025-04-03 18:06:44 -0400

    Removes unneeded export

--- src/plus/ai/models/model.ts
+++ src/plus/ai/models/model.ts
@@ -29,7 +29,7 @@ export type AIActionType =
 	| 'generate-stashMessage'
 	| 'generate-changelog'
 	| `generate-create-${'cloudPatch' | 'codeSuggestion' | 'pullRequest'}`
-	| `explain-changes`;
+	| 'explain-changes';
 
 export interface AIProviderConstructor<Provider extends AIProviders = AIProviders> {
 	new (container: Container, connection: ServerConnection): AIProvider<Provider>;
