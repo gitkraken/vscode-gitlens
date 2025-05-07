13746b6a94351dd4ef311e7b7f2cf353199f5030
Keith Daulton <keith.daulton@gitkraken.com>
2025-05-07 18:49:32 -0400

    Updates explain branch
    - improves markdown format
    - moves prepareCompareDataForAIRequest out of AIProviderService

--- src/plus/ai/aiProviderService.ts
+++ src/plus/ai/aiProviderService.ts
@@ -626,63 +626,6 @@ export class AIProviderService implements Disposable {
 		return result != null ? { ...result, parsed: parseSummarizeResult(result.content) } : undefined;
 	}
 
-	async prepareCompareDataForAIRequest(
-		repo: Repository,
-		headRef: string,
-		baseRef: string,
-		options?: {
-			cancellation?: CancellationToken;
-			reportNoDiffService?: () => void;
-			reportNoCommitsService?: () => void;
-			reportNoChanges?: () => void;
-		},
-	): Promise<{ diff: string; logMessages: string } | undefined> {
-		const { cancellation, reportNoDiffService, reportNoCommitsService, reportNoChanges } = options ?? {};
-		const diffService = repo.git.diff();
-		if (diffService?.getDiff === undefined) {
-			if (reportNoDiffService) {
-				reportNoDiffService();
-				return;
-			}
-		}
-
-		const commitsService = repo.git.commits();
-		if (commitsService?.getLog === undefined) {
-			if (reportNoCommitsService) {
-				reportNoCommitsService();
-				return;
-			}
-		}
-
-		const [diffResult, logResult] = await Promise.allSettled([
-			diffService.getDiff?.(headRef, baseRef, { notation: '...' }),
-			commitsService.getLog(`${baseRef}..${headRef}`),
-		]);
-		const diff = getSettledValue(diffResult);
-		const log = getSettledValue(logResult);
-
-		if (!diff?.contents || !log?.commits?.size) {
-			reportNoChanges?.();
-			return undefined;
-		}
-
-		if (cancellation?.isCancellationRequested) throw new CancellationError();
-
-		const commitMessages: string[] = [];
-		for (const commit of [...log.commits.values()].sort((a, b) => a.date.getTime() - b.date.getTime())) {
-			const message = commit.message ?? commit.summary;
-			if (message) {
-				commitMessages.push(
-					`<commit-message ${commit.date.toISOString()}>\n${
-						commit.message ?? commit.summary
-					}\n<end-of-commit-message>`,
-				);
-			}
-		}
-
-		return { diff: diff.contents, logMessages: commitMessages.join('\n\n') };
-	}
-
 	async generateCreatePullRequest(
 		repo: Repository,
 		baseRef: string,
@@ -698,7 +641,7 @@ export class AIProviderService implements Disposable {
 		const result = await this.sendRequest(
 			'generate-create-pullRequest',
 			async (model, reporting, cancellation, maxInputTokens, retries) => {
-				const compareData = await this.prepareCompareDataForAIRequest(repo, headRef, baseRef, {
+				const compareData = await prepareCompareDataForAIRequest(repo, headRef, baseRef, {
 					cancellation: cancellation,
 				});
 
@@ -1451,4 +1394,61 @@ function isPrimaryAIProvider(provider: AIProviders): provider is AIPrimaryProvid
 
 function isPrimaryAIProviderModel(model: AIModel): model is AIModel<AIPrimaryProviders, AIProviderAndModel> {
 	return isPrimaryAIProvider(model.provider.id);
+}
+
+export async function prepareCompareDataForAIRequest(
+	repo: Repository,
+	headRef: string,
+	baseRef: string,
+	options?: {
+		cancellation?: CancellationToken;
+		reportNoDiffService?: () => void;
+		reportNoCommitsService?: () => void;
+		reportNoChanges?: () => void;
+	},
+): Promise<{ diff: string; logMessages: string } | undefined> {
+	const { cancellation, reportNoDiffService, reportNoCommitsService, reportNoChanges } = options ?? {};
+	const diffService = repo.git.diff();
+	if (diffService?.getDiff === undefined) {
+		if (reportNoDiffService) {
+			reportNoDiffService();
+			return;
+		}
+	}
+
+	const commitsService = repo.git.commits();
+	if (commitsService?.getLog === undefined) {
+		if (reportNoCommitsService) {
+			reportNoCommitsService();
+			return;
+		}
+	}
+
+	const [diffResult, logResult] = await Promise.allSettled([
+		diffService.getDiff?.(headRef, baseRef, { notation: '...' }),
+		commitsService.getLog(`${baseRef}..${headRef}`),
+	]);
+	const diff = getSettledValue(diffResult);
+	const log = getSettledValue(logResult);
+
+	if (!diff?.contents || !log?.commits?.size) {
+		reportNoChanges?.();
+		return undefined;
+	}
+
+	if (cancellation?.isCancellationRequested) throw new CancellationError();
+
+	const commitMessages: string[] = [];
+	for (const commit of [...log.commits.values()].sort((a, b) => a.date.getTime() - b.date.getTime())) {
+		const message = commit.message ?? commit.summary;
+		if (message) {
+			commitMessages.push(
+				`<commit-message ${commit.date.toISOString()}>\n${
+					commit.message ?? commit.summary
+				}\n<end-of-commit-message>`,
+			);
+		}
+	}
+
+	return { diff: diff.contents, logMessages: commitMessages.join('\n\n') };
 }
