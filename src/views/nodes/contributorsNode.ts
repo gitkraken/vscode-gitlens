4edd7534cfc3e95986fbd372f2d0eb701d3108ec
Eric Amodio <eamodio@gmail.com>
2025-05-31 00:54:31 -0400

    Improves contributors view performance w/ stats  - Defers stats loading to improve initial load performance  - Adds loading indicator when stats are being computed in background

--- src/views/nodes/contributorsNode.ts
+++ src/views/nodes/contributorsNode.ts
@@ -16,7 +16,7 @@ import { ContributorNode } from './contributorNode';
 export class ContributorsNode extends CacheableChildrenViewNode<
 	'contributors',
 	ViewsWithContributorsNode,
-	ContributorNode
+	ContributorNode | MessageNode | ActionMessageNode
 > {
 	constructor(
 		uri: GitUri,
@@ -47,81 +47,16 @@ export class ContributorsNode extends CacheableChildrenViewNode<
 
 	async getChildren(): Promise<ViewNode[]> {
 		if (this.children == null) {
-			let rev = this.options?.ref;
-			const all = rev == null && (this.options?.all ?? configuration.get('views.contributors.showAllBranches'));
-
-			const svc = this.view.container.git.getRepositoryService(this.uri.repoPath!);
-
-			// If there is no ref and we aren't getting all branches, get the upstream of the current branch if there is one
-			if (rev == null && !all) {
-				try {
-					const branch = await svc.branches.getBranch();
-					if (branch?.upstream?.name != null && !branch.upstream.missing) {
-						rev = '@{u}';
-					}
-				} catch {}
-			}
-
 			const stats = this.options?.stats ?? configuration.get('views.contributors.showStatistics');
+			const children = await this.getContributors(stats, true);
 
-			let timeout: number;
-			const overrideMaxWait = this.getState('overrideMaxWait');
-			if (overrideMaxWait) {
-				timeout = overrideMaxWait;
-				this.deleteState('overrideMaxWait');
-			} else {
-				timeout = configuration.get('views.contributors.maxWait') * 1000;
+			if (stats) {
+				queueMicrotask(async () => {
+					this.children = await this.getContributors(true, false);
+					void this.triggerChange();
+				});
 			}
 
-			const result = await svc.contributors.getContributors(
-				rev,
-				{ all: all, merges: this.options?.showMergeCommits, stats: stats },
-				undefined,
-				timeout || undefined,
-			);
-			if (!result.contributors.length) {
-				return [new MessageNode(this.view, this, 'No contributors could be found.')];
-			}
-
-			const children: ContributorNode[] = [];
-			if (result.cancelled) {
-				children.push(
-					new ActionMessageNode(
-						this.view,
-						this,
-						n => {
-							n.update({
-								iconPath: new ThemeIcon('loading~spin'),
-								message: 'Loading contributors...',
-								description: `waiting for ${(timeout * 2) / 1000}s`,
-								tooltip: null,
-							});
-							this.storeState('overrideMaxWait', timeout * 2);
-							void this.triggerChange(true);
-						},
-						stats ? 'Showing incomplete contributors and statistics' : 'Showing incomplete contributors',
-						result.cancelled.reason === 'timedout' ? `timed out after ${timeout / 1000}s` : 'cancelled',
-						'Click to retry and wait longer for contributors',
-						new ThemeIcon('warning', new ThemeColor('list.warningForeground' satisfies CoreColors)),
-					) as unknown as ContributorNode,
-				);
-			}
-
-			sortContributors(result.contributors);
-			const presenceMap = this.view.container.vsls.active
-				? await this.getPresenceMap(result.contributors)
-				: undefined;
-
-			for (const c of result.contributors) {
-				children.push(
-					new ContributorNode(this.uri, this.view, this, c, {
-						all: all,
-						ref: rev,
-						presence: presenceMap,
-						showMergeCommits: this.options?.showMergeCommits,
-					}),
-				);
-			}
 			this.children = children;
 		}
 
@@ -142,6 +77,8 @@ export class ContributorsNode extends CacheableChildrenViewNode<
 		if (this.children == null) return;
 
 		for (const child of this.children) {
+			if (!child.is('contributor')) continue;
+
 			if (child.contributor.email === email) {
 				void child.triggerChange();
 			}
@@ -155,5 +92,99 @@ export class ContributorsNode extends CacheableChildrenViewNode<
 		if (email == null) return undefined;
 
 		return this.view.container.vsls.getContactsPresence([email]);
+	}
+
+	private async getContributors(
+		stats?: boolean,
+		deferStats?: boolean,
+	): Promise<(ContributorNode | MessageNode | ActionMessageNode)[]> {
+		let rev = this.options?.ref;
+		const all = rev == null && (this.options?.all ?? configuration.get('views.contributors.showAllBranches'));
+
+		const svc = this.view.container.git.getRepositoryService(this.uri.repoPath!);
+
+		// If there is no ref and we aren't getting all branches, get the upstream of the current branch if there is one
+		if (rev == null && !all) {
+			try {
+				const branch = await svc.branches.getBranch();
+				if (branch?.upstream?.name != null && !branch.upstream.missing) {
+					rev = '@{u}';
+				}
+			} catch {}
+		}
+
+		let timeout: number;
+		const overrideMaxWait = this.getState('overrideMaxWait');
+		if (overrideMaxWait) {
+			timeout = overrideMaxWait;
+			this.deleteState('overrideMaxWait');
+		} else {
+			timeout = configuration.get('views.contributors.maxWait') * 1000;
+		}
+
+		const result = await svc.contributors.getContributors(
+			rev,
+			{ all: all, merges: this.options?.showMergeCommits, stats: !deferStats && stats },
+			this.view.cancellation,
+			timeout || undefined,
+		);
+		if (!result.contributors.length) {
+			return [new MessageNode(this.view, this, 'No contributors could be found.')];
+		}
+
+		const children: (ContributorNode | MessageNode | ActionMessageNode)[] = [];
+		if (result.cancelled) {
+			children.push(
+				new ActionMessageNode(
+					this.view,
+					this,
+					n => {
+						n.update({
+							iconPath: new ThemeIcon('loading~spin'),
+							message: 'Loading contributors...',
+							description: `waiting for ${(timeout * 2) / 1000}s`,
+							tooltip: null,
+						});
+						this.storeState('overrideMaxWait', timeout * 2);
+						void this.triggerChange(true);
+					},
+					stats ? 'Showing incomplete contributors and statistics' : 'Showing incomplete contributors',
+					result.cancelled.reason === 'timedout' ? `timed out after ${timeout / 1000}s` : 'cancelled',
+					'Click to retry and wait longer for contributors',
+					new ThemeIcon('warning', new ThemeColor('list.warningForeground' satisfies CoreColors)),
+				),
+			);
+		}
+
+		if (deferStats && stats) {
+			children.push(
+				new MessageNode(
+					this.view,
+					this,
+					'Loading statistics...',
+					undefined,
+					undefined,
+					new ThemeIcon('loading~spin'),
+				),
+			);
+		}
+
+		sortContributors(result.contributors);
+		const presenceMap = this.view.container.vsls.active
+			? await this.getPresenceMap(result.contributors)
+			: undefined;
+
+		for (const c of result.contributors) {
+			children.push(
+				new ContributorNode(this.uri, this.view, this, c, {
+					all: all,
+					ref: rev,
+					presence: presenceMap,
+					showMergeCommits: this.options?.showMergeCommits,
+				}),
+			);
+		}
+
+		return children;
 	}
 }
