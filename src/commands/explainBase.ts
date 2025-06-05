52680eaaeda4a2547b3ded21443d298ba641bf2e
Keith Daulton <keith.daulton@gitkraken.com>
2025-06-05 16:33:10 -0400

    Adds virtual document to base explain

--- src/commands/explainBase.ts
+++ src/commands/explainBase.ts
@@ -1,10 +1,13 @@
 import type { TextEditor, Uri } from 'vscode';
 import type { GlCommands } from '../constants.commands';
 import type { Container } from '../container';
+import type { MarkdownContentMetadata } from '../documents/markdown';
+import { getMarkdownHeaderContent } from '../documents/markdown';
 import type { GitRepositoryService } from '../git/gitRepositoryService';
 import { GitUri } from '../git/gitUri';
-import type { AIExplainSource } from '../plus/ai/aiProviderService';
+import type { AIExplainSource, AISummarizeResult } from '../plus/ai/aiProviderService';
 import { getBestRepositoryOrShowPicker } from '../quickpicks/repositoryPicker';
+import { showMarkdownPreview } from '../system/-webview/markdown';
 import { GlCommandBase } from './commandBase';
 import { getCommandUri } from './commandBase.utils';
 
@@ -49,5 +52,13 @@ export abstract class ExplainCommandBase extends GlCommandBase {
 		}
 
 		return svc;
+	}
+
+	protected openDocument(result: AISummarizeResult, path: string, metadata: MarkdownContentMetadata): void {
+		const content = `${getMarkdownHeaderContent(metadata)}\n\n${result.parsed.summary}\n\n${result.parsed.body}`;
+
+		const documentUri = this.container.markdown.openDocument(content, path, metadata.header.title, metadata);
+
+		showMarkdownPreview(documentUri);
 	}
 }
