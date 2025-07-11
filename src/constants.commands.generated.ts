e6633c2e215d61d6eedd35973992fa55ce64aef7
Keith Daulton <keith.daulton@gitkraken.com>
2025-07-11 18:38:19 -0400

    Adds starter webview for the composer (wip)

--- src/constants.commands.generated.ts
+++ src/constants.commands.generated.ts
@@ -788,6 +788,7 @@ export type ContributedPaletteCommands =
 	| 'gitlens.showCommitSearch'
 	| 'gitlens.showCommitsInView'
 	| 'gitlens.showCommitsView'
+	| 'gitlens.showComposerPage'
 	| 'gitlens.showContributorsView'
 	| 'gitlens.showDraftsView'
 	| 'gitlens.showFileHistoryView'
