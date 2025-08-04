6a9e3bc161cb5e4db793ff1f6d468ccf8f33ade1
Eric Amodio <eamodio@gmail.com>
2025-08-04 14:48:34 -0400

    Updates dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -344,6 +344,7 @@ export default ts.config(
 			'@typescript-eslint/no-restricted-imports': restrictedImports.extension,
 			'@typescript-eslint/no-unnecessary-condition': 'off',
 			'@typescript-eslint/no-unnecessary-boolean-literal-compare': 'off',
+			'@typescript-eslint/no-unnecessary-type-conversion': 'off',
 			'@typescript-eslint/no-unnecessary-type-parameters': 'off', // https://github.com/typescript-eslint/typescript-eslint/issues/9705
 			'@typescript-eslint/no-unsafe-argument': 'off',
 			'@typescript-eslint/no-unsafe-assignment': 'off',
