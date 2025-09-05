421cb8473017ade96d92012414a15bc5ffcaf9a8
Keith Daulton <keith.daulton@gitkraken.com>
2025-09-05 18:08:42 -0400

    Update provideMcpServerDefinitions to get MCP config from CLI

--- src/env/node/gk/mcp/integration.ts
+++ src/env/node/gk/mcp/integration.ts
@@ -1,21 +1,18 @@
 import type { Event, McpServerDefinition } from 'vscode';
-import {
-	version as codeVersion,
-	Disposable,
-	env,
-	EventEmitter,
-	lm,
-	McpStdioServerDefinition,
-	Uri,
-	window,
-} from 'vscode';
+import { version as codeVersion, Disposable, env, EventEmitter, lm, McpStdioServerDefinition, window } from 'vscode';
 import type { Container } from '../../../../container';
 import type { StorageChangeEvent } from '../../../../system/-webview/storage';
 import { getHostAppName } from '../../../../system/-webview/vscode';
 import { debounce } from '../../../../system/function/debounce';
+import { Logger } from '../../../../system/logger';
 import { satisfies } from '../../../../system/version';
+import { run } from '../../git/shell';
 import { getPlatform } from '../../platform';
 import { toMcpInstallProvider } from './utils';
+
+const CLIProxyMCPConfigOutputs = {
+	checkingForUpdates: /checking for updates.../i,
+};
 
 export class McpProvider implements Disposable {
 	static #instance: McpProvider | undefined;
@@ -54,7 +51,7 @@ export class McpProvider implements Disposable {
 	}
 
 	private async provideMcpServerDefinitions(): Promise<McpServerDefinition[]> {
-		const config = await this.getMcpConfiguration();
+		const config = await this.getMcpConfigurationFromCLI();
 		if (config == null) {
 			return [];
 		}
@@ -72,7 +69,32 @@ export class McpProvider implements Disposable {
 		return [serverDefinition];
 	}
 
-	private async getMcpConfiguration(): Promise<
+	// private async getMcpConfiguration(): Promise<
+	// 	{ name: string; type: string; command: string; args: string[]; version?: string } | undefined
+	// > {
+	// 	const cliInstall = this.container.storage.get('gk:cli:install');
+	// 	const cliPath = this.container.storage.get('gk:cli:path');
+
+	// 	if (cliInstall?.status !== 'completed' || !cliPath) {
+	// 		return undefined;
+	// 	}
+
+	// 	const platform = getPlatform();
+	// 	const executable = platform === 'windows' ? 'gk.exe' : 'gk';
+	// 	const command = Uri.joinPath(Uri.file(cliPath), executable);
+
+	// 	const appName = toMcpInstallProvider(await getHostAppName());
+	// 	const args = ['mcp', `--host=${appName}`, '--source=gitlens', `--scheme=${env.uriScheme}`];
+	// 	return {
+	// 		name: 'GitKraken MCP Server',
+	// 		type: 'stdio',
+	// 		command: command.fsPath,
+	// 		args: args,
+	// 		version: cliInstall.version,
+	// 	};
+	// }
+
+	private async getMcpConfigurationFromCLI(): Promise<
 		{ name: string; type: string; command: string; args: string[]; version?: string } | undefined
 	> {
 		const cliInstall = this.container.storage.get('gk:cli:install');
@@ -82,19 +104,50 @@ export class McpProvider implements Disposable {
 			return undefined;
 		}
 
-		const platform = getPlatform();
-		const executable = platform === 'windows' ? 'gk.exe' : 'gk';
-		const command = Uri.joinPath(Uri.file(cliPath), executable);
-
 		const appName = toMcpInstallProvider(await getHostAppName());
-		const args = ['mcp', `--host=${appName}`, '--source=gitlens', `--scheme=${env.uriScheme}`];
-		return {
-			name: 'GitKraken MCP Server',
-			type: 'stdio',
-			command: command.fsPath,
-			args: args,
-			version: cliInstall.version,
-		};
+		if (appName == null) {
+			return undefined;
+		}
+
+		let output = await this.runCLICommand(
+			['mcp', 'config', appName, '--source=gitlens', `--scheme=${env.uriScheme}`],
+			{
+				cwd: cliPath,
+			},
+		);
+		output = output.replace(CLIProxyMCPConfigOutputs.checkingForUpdates, '').trim();
+		console.log(output);
+
+		try {
+			const configuration = JSON.parse(output) as { name: string; type: string; command: string; args: string[] };
+
+			return {
+				name: configuration.name,
+				type: configuration.type,
+				command: configuration.command,
+				args: configuration.args,
+				version: cliInstall.version,
+			};
+		} catch (ex) {
+			Logger.error(`Error getting MCP configuration: ${ex}`);
+		}
+
+		return undefined;
+	}
+
+	private async runCLICommand(
+		args: string[],
+		options?: {
+			cwd?: string;
+		},
+	): Promise<string> {
+		const platform = getPlatform();
+		const cwd = options?.cwd ?? this.container.storage.get('gk:cli:path');
+		if (cwd == null) {
+			throw new Error('CLI is not installed');
+		}
+
+		return run(platform === 'windows' ? 'gk.exe' : './gk', args, 'utf8', { cwd: cwd });
 	}
 
 	dispose(): void {
