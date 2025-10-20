gitkraken/vscode-gitlens

commit 4f0c42439f5f04166d6377ea386cc78f5a9f373d

Adds additional logging

src/views/nodes/abstract/subscribeableViewNode.ts
@@ -5,6 +5,7 @@ import type { GitUri } from '../../../git/gitUri';
 import { gate } from '../../../system/decorators/gate';
 import { debug } from '../../../system/decorators/log';
 import { weakEvent } from '../../../system/event';
+import { getLogScope, setLogScopeExit } from '../../../system/logger.scope';
 import type { View } from '../../viewBase';
 import { CacheableChildrenViewNode } from './cacheableChildrenViewNode';
 import type { ViewNode } from './viewNode';
@@ -144,8 +145,20 @@ export abstract class SubscribeableViewNode<
 	@gate()
 	@debug()
 	async ensureSubscription(): Promise<void> {
+		const scope = getLogScope();
+
 		// We only need to subscribe if we are visible and if auto-refresh enabled (when supported)
-		if (!this.canSubscribe || !this.view.visible || (canAutoRefreshView(this.view) && !this.view.autoRefresh)) {
+		const {
+			canSubscribe,
+			view: { visible: isVisible },
+		} = this;
+		const canAutoRefresh = canAutoRefreshView(this.view) && this.view.autoRefresh;
+
+		if (!canSubscribe || !isVisible || !canAutoRefresh) {
+			setLogScopeExit(
+				scope,
+				` \u2022 unsubscribed (subscription=${this.subscription != null}); canSubscribe=${canSubscribe}, viewVisible=${isVisible}, canAutoRefresh=${canAutoRefresh}`,
+			);
 			await this.unsubscribe();
 
 			return;
@@ -153,6 +166,11 @@ export abstract class SubscribeableViewNode<
 
 		// If we already have a subscription, just kick out
 		if (this.subscription != null) return;
+
+		setLogScopeExit(
+			scope,
+			` \u2022 subscribed; canSubscribe=${canSubscribe}, viewVisible=${isVisible}, canAutoRefresh=${canAutoRefresh}`,
+		);
 
 		this.subscription = Promise.resolve(this.subscribe());
 		void (await this.subscription);

src/git/models/repository.ts
@@ -679,18 +679,22 @@ export class Repository implements Disposable {
 		);
 	}
 
+	@debug({ singleLine: true })
 	resume(): void {
 		if (!this._suspended) return;
 
+		const scope = getLogScope();
 		this._suspended = false;
 
 		// If we've come back into focus and we are dirty, fire the change events
 
 		if (this._pendingRepoChange != null) {
-			this._fireChangeDebounced!();
+			Logger.debug(scope, `Firing pending repo changes: ${this._pendingRepoChange.toString(true)}`);
+			this._fireChangeDebounced?.();
 		}
 
 		if (this._pendingFileSystemChange != null) {
+			Logger.debug(scope, `Firing pending file system changes`);
 			this._fireFileSystemChangeDebounced?.();
 		}
 	}
@@ -705,6 +709,7 @@ export class Repository implements Disposable {
 		return starred != null && starred[this.id] === true;
 	}
 
+	@log<Repository['star']>({ args: { 0: b => b?.name } })
 	star(branch?: GitBranch): Promise<void> {
 		return this.updateStarred(true, branch);
 	}
@@ -743,6 +748,7 @@ export class Repository implements Disposable {
 		return rev != null ? this.git.getBestRevisionUri(path, rev) : undefined;
 	}
 
+	@log<Repository['unstar']>({ args: { 0: b => b?.name } })
 	unstar(branch?: GitBranch): Promise<void> {
 		return this.updateStarred(false, branch);
 	}
@@ -771,6 +777,7 @@ export class Repository implements Disposable {
 		return this._etagFileSystem;
 	}
 
+	@debug({ singleLine: true })
 	suspend(): void {
 		this._suspended = true;
 	}
@@ -808,6 +815,7 @@ export class Repository implements Disposable {
 	private _fsWatchers = new Map<string, number>();
 	private _fsChangeDelay: number = defaultFileSystemChangeDelay;
 
+	@debug({ singleLine: true })
 	watchFileSystem(delay: number = defaultFileSystemChangeDelay): Disposable {
 		const id = uuid();
 		this._fsWatchers.set(id, delay);
@@ -867,7 +875,7 @@ export class Repository implements Disposable {
 		this.providerService.onRepositoryChanged(this, this._pendingRepoChange);
 
 		if (this._suspended) {
-			Logger.debug(scope, `queueing suspended ${this._pendingRepoChange.toString(true)}`);
+			Logger.debug(scope, `SUSPENDED: queueing ${this._pendingRepoChange.toString(true)}`);
 
 			return;
 		}

src/system/event.ts
@@ -1,4 +1,5 @@
 import type { Disposable, Event } from 'vscode';
+import { Logger } from './logger';
 import type { Deferred } from './promise';
 
 export function once<T>(event: Event<T>): Event<T> {
@@ -112,21 +113,34 @@ export function weakEvent<T, U extends object>(
 ): Disposable {
 	const ref = new WeakRef<U>(thisArg);
 
+	let disposed = false;
 	let disposable: Disposable;
 
 	const d = event((e: T) => {
 		const obj = ref.deref();
 		if (obj != null) {
 			listener.call(obj, e);
-		} else {
+		} else if (!disposed) {
+			Logger.warn(`weakEvent GC'd; disposing listener`);
 			disposable.dispose();
 		}
 	});
 
 	if (alsoDisposeOnReleaseOrDispose == null) {
-		disposable = d;
+		disposable = {
+			dispose: () => {
+				disposed = true;
+				d.dispose();
+			},
+		};
 	} else {
-		disposable = disposableFrom(d, ...alsoDisposeOnReleaseOrDispose);
+		const wrapped = disposableFrom(d, ...alsoDisposeOnReleaseOrDispose);
+		disposable = {
+			dispose: () => {
+				disposed = true;
+				wrapped.dispose();
+			},
+		};
 	}
 	return disposable;
 }

commit efe604a3c2c32db20e661e17154b59b6f3540a8f

Fixes logic error in subscription Introduced in commit 4f0c42439f5f04166d6377ea386cc78f5a9f373d

src/views/nodes/abstract/subscribeableViewNode.ts
@@ -147,17 +147,17 @@ export abstract class SubscribeableViewNode<
 	async ensureSubscription(): Promise<void> {
 		const scope = getLogScope();
 
-		// We only need to subscribe if we are visible and if auto-refresh enabled (when supported)
+		// We only need to subscribe if we are visible and if auto-refresh isn't disabled
 		const {
 			canSubscribe,
 			view: { visible: isVisible },
 		} = this;
-		const canAutoRefresh = canAutoRefreshView(this.view) && this.view.autoRefresh;
+		const autoRefreshDisabled = canAutoRefreshView(this.view) && !this.view.autoRefresh;
 
-		if (!canSubscribe || !isVisible || !canAutoRefresh) {
+		if (!canSubscribe || !isVisible || autoRefreshDisabled) {
 			setLogScopeExit(
 				scope,
-				` \u2022 unsubscribed (subscription=${this.subscription != null}); canSubscribe=${canSubscribe}, viewVisible=${isVisible}, canAutoRefresh=${canAutoRefresh}`,
+				` \u2022 unsubscribed (subscription=${this.subscription != null}); canSubscribe=${canSubscribe}, viewVisible=${isVisible}, autoRefreshDisabled=${autoRefreshDisabled}`,
 			);
 			await this.unsubscribe();
 
@@ -169,7 +169,7 @@ export abstract class SubscribeableViewNode<
 
 		setLogScopeExit(
 			scope,
-			` \u2022 subscribed; canSubscribe=${canSubscribe}, viewVisible=${isVisible}, canAutoRefresh=${canAutoRefresh}`,
+			` \u2022 subscribed; canSubscribe=${canSubscribe}, viewVisible=${isVisible}, autoRefreshDisabled=${autoRefreshDisabled}`,
 		);
 
 		this.subscription = Promise.resolve(this.subscribe());