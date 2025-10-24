dac329decb2c9ef3a5482c027733e76466d6a122
Eric Amodio <eamodio@gmail.com>
2025-10-24 18:01:41 -0400

    Fixes stuck indicator on Visual History

--- src/webviews/apps/plus/timeline/components/chart.ts
+++ src/webviews/apps/plus/timeline/components/chart.ts
@@ -173,7 +173,11 @@ export class GlTimelineChart extends GlElement {
 			this.emit('gl-loading', this._loading.promise);
 		}
 
-		if (this.dataPromise == null) return;
+		if (this.dataPromise == null) {
+			// Fulfill the loading promise since there's no data to load
+			this._loading?.fulfill();
+			return;
+		}
 
 		this._chartAborter?.abort();
 		this._chartAborter = new AbortController();
