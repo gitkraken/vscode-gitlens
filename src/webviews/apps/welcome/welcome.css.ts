0f225f6015a7c55cf2d786c587cdacbf0a43bba0
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2025-12-02 18:20:28 +0100

    Layout of the header of the welcome page
    (#4769, #4773, PLG-138)

---
+++ src/webviews/apps/welcome/welcome.css.ts
@@ -0,0 +1,51 @@
+import { css } from 'lit';
+
+export const welcomeStyles = css`
+	.welcome::before {
+		content: ' ';
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%) translateY(-40%);
+		z-index: -1;
+
+		width: 620px;
+		height: 517px;
+		max-width: 100%;
+
+		border-radius: 100%;
+		background: radial-gradient(76.32% 76.32% at 50% 7.24%, #7b00ff 29.72%, rgba(255, 0, 242, 0) 100%);
+		opacity: 0.25;
+		mix-blend-mode: color;
+		filter: blur(53px);
+	}
+	.welcome__section {
+		display: flex;
+		flex-flow: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+	}
+	.welcome__section p {
+		font-size: larger;
+		max-width: calc(620px * 0.75);
+	}
+
+	.welcome__header {
+		margin-top: 5rem;
+		margin-bottom: 2rem;
+		max-width: 620px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.welcome__header gitlens-logo {
+		transform: translateX(-0.75rem);
+	}
+	.welcome__header h1 {
+		margin-bottom: 0;
+	}
+
+	.welcome__accent {
+		color: #cb64ff;
+	}
+`;
