56741fbdc339903a2038c70fc9fac177f687a49e
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2025-12-04 17:00:36 +0100

    Makes header of the welcome page responsive to the panel width (#4769, #4773, PLG-138)

--- src/webviews/apps/welcome/welcome.css.ts
+++ src/webviews/apps/welcome/welcome.css.ts
@@ -3,6 +3,22 @@ import { css } from 'lit';
 const colorScheme = css`
 	:host {
 		--accent-color: #cb64ff;
+
+		--hero-gradient: radial-gradient(76.32% 76.32% at 50% 7.24%, #7b00ff 29.72%, rgba(255, 0, 242, 0) 100%);
+	}
+`;
+
+const typography = css`
+	:host {
+		--h1-font-size: 1.4rem;
+		--p-font-size: 1rem;
+	}
+
+	@media (max-width: 640px) {
+		:host {
+			--h1-font-size: 0.75rem;
+			--p-font-size: 0.7rem;
+		}
 	}
 `;
 
@@ -15,15 +31,20 @@ const heroGradient = css`
 		transform: translateX(-50%) translateY(-40%);
 		z-index: -1;
 
+		background: var(--hero-gradient);
+		border-radius: 100%;
+		opacity: 0.25;
+		filter: blur(53px);
+
 		width: 620px;
 		height: 517px;
 		max-width: 100%;
+	}
 
-		border-radius: 100%;
-		background: radial-gradient(76.32% 76.32% at 50% 7.24%, #7b00ff 29.72%, rgba(255, 0, 242, 0) 100%);
-		opacity: 0.25;
-		mix-blend-mode: color;
-		filter: blur(53px);
+	@media (max-width: 640px) {
+		.welcome::before {
+			height: 273px;
+		}
 	}
 `;
 
@@ -34,17 +55,29 @@ const section = css`
 		justify-content: center;
 		align-items: center;
 		text-align: center;
+		font-size: var(--p-font-size);
 	}
 	.section p {
-		font-size: larger;
-		max-width: calc(620px * 0.75);
+		max-width: 30em;
+	}
+	.section .accent {
+		color: var(--accent-color);
 	}
 `;
 
 const header = css`
+	.logo {
+		transform: scale(0.7);
+	}
+	@media (max-width: 640px) {
+		.logo {
+			transform: scale(0.5);
+		}
+	}
+
 	.header {
-		margin-top: 5rem;
-		margin-bottom: 2rem;
+		margin-top: 3em;
+		margin-bottom: 1em;
 		max-width: 620px;
 		margin-left: auto;
 		margin-right: auto;
@@ -54,16 +87,15 @@ const header = css`
 	}
 	.header h1 {
 		margin-bottom: 0;
-	}
-`;
 
-const typography = css`
-	.accent {
-		color: var(--accent-color);
+		font-size: var(--h1-font-size);
+	}
+	.header p {
+		color: var(--vscode-descriptionForeground);
 	}
 `;
 
 export const welcomeStyles = css`
-	${colorScheme}
-	${heroGradient} ${section} ${header} ${typography}
+	${colorScheme} ${typography}
+	${heroGradient} ${section} ${header}
 `;
