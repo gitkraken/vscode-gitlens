98f373443f74041841835bb0f3f27752fc93b3ee
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2025-12-29 15:04:04 +0100

    Updates "pr-reviews" (launchpad) section of the Walkthrough
    PLG-121, #4842, #4846

--- src/constants.walkthroughs.ts
+++ src/constants.walkthroughs.ts
@@ -11,7 +11,7 @@ export const walkthroughProgressSteps: Record<WalkthroughContextKeys, string> = 
 	gettingStarted: 'Getting Started',
 	homeView: 'Home View',
 	visualizeCodeHistory: 'Visualize Code History',
-	prReviews: 'PR Reviews',
+	prReviews: 'Launchpad',
 	streamlineCollaboration: 'Streamline Collaboration',
 	integrations: 'Integrations',
 	aiFeatures: 'AI Features',
