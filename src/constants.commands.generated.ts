3f6c4af7e098c576f40a1c01ab085ace03440b7f
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2026-02-06 19:46:37 +0100

    Adds refresh button to Welcome view
    Provides a dedicated button in the Welcome view's title bar to manually refresh its content. This enhances user control and helps ensure the view displays the most current information.
    
    (gitkraken/vscode-gitlens-private#81, #4970, PLG-164)

--- src/constants.commands.generated.ts
+++ src/constants.commands.generated.ts
@@ -788,6 +788,7 @@ export type ContributedCommands =
 	| 'gitlens.views.unstageFile'
 	| 'gitlens.views.unstageFile:commitDetails'
 	| 'gitlens.views.unstageFile:graphDetails'
+	| 'gitlens.views.welcome.refresh'
 	| 'gitlens.views.workspaces.addRepos'
 	| 'gitlens.views.workspaces.addReposFromLinked'
 	| 'gitlens.views.workspaces.changeAutoAddSetting'
