60e0f9c19254e346a4b7192fcc869739df7a36bc
Keith Daulton <keith.daulton@gitkraken.com>
2026-02-10 17:04:07 -0500

    Adds a Start Review description to the account access gate

--- src/commands/quick-wizard/steps/access.ts
+++ src/commands/quick-wizard/steps/access.ts
@@ -103,6 +103,17 @@ export async function* ensureAccessStep<
 				createQuickPickSeparator(),
 			);
 			break;
+		case 'startReview':
+			directives.splice(
+				0,
+				0,
+				createDirectiveQuickPickItem(Directive.Noop, undefined, {
+					label: 'Start reviewing a pull request from your connected integrations',
+					iconPath: new ThemeIcon('git-pull-request'),
+				}),
+				createQuickPickSeparator(),
+			);
+			break;
 		case 'startWork':
 			directives.splice(
 				0,
