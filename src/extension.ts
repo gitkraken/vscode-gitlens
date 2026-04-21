6a19d8eb8285d2729873247ca728f3fdcb63a612
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2026-04-21 15:46:46 +0200

    Enhances custom avatar functionality with approval system for URL templates (#5155)

--- src/extension.ts
+++ src/extension.ts
@@ -303,7 +303,12 @@ export function deactivate(): void {
 // }
 
 function setKeysForSync(context: ExtensionContext, ...keys: (SyncedStorageKeys | string)[]) {
-	context.globalState?.setKeysForSync([...keys, SyncedStorageKeys.Version, SyncedStorageKeys.PreReleaseVersion]);
+	context.globalState?.setKeysForSync([
+		...keys,
+		SyncedStorageKeys.ApprovedAvatarRemoteTemplates,
+		SyncedStorageKeys.Version,
+		SyncedStorageKeys.PreReleaseVersion,
+	]);
 }
 
 function registerBuiltInActionRunners(container: Container): void {
