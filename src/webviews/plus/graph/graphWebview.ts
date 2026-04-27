b5c1e76f4b918b1ad14d163c9cddba78e685bfda
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2026-04-27 16:29:24 +0200

    Refactors jump to reference functionality in GlGraphHeader and adds notification for pinned reference changes in GraphWebviewProvider
    (#5139, #5181)

--- src/webviews/plus/graph/graphWebview.ts
+++ src/webviews/plus/graph/graphWebview.ts
@@ -484,6 +484,7 @@ export class GraphWebviewProvider implements WebviewProvider<State, State, Graph
 		[DidChangeGraphConfigurationNotification, this.notifyDidChangeConfiguration],
 		[DidChangeNotification, this.notifyDidChangeState],
 		[DidChangeOverviewNotification, this.notifyDidChangeOverview],
+		[DidChangePinnedRefNotification, this.notifyDidChangePinnedRef],
 		[DidChangeRefsVisibilityNotification, this.notifyDidChangeRefsVisibility],
 		[DidChangeScrollMarkersNotification, this.notifyDidChangeScrollMarkers],
 		[DidChangeSelectionNotification, this.notifyDidChangeSelection],
@@ -5586,7 +5587,7 @@ export class GraphWebviewProvider implements WebviewProvider<State, State, Graph
 
 		void this.updateFiltersByRepo(repoPath, { pinnedRef: storedPinnedRef });
 		void this.notifyDidChangePinnedRef();
-		this.updateState();
+		this.updateState(true);
 	}
 
 	private updateFiltersByRepo(repoPath: string | undefined, updates: Partial<StoredGraphFilters>) {
