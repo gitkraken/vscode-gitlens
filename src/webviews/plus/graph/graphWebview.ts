f646c8b8b9e1fdd1eeb074f898cfd1059e328ed4
Sergei Shmakov <sergei.shmakov@gitkraken.com>
2026-05-11 21:58:40 +0200

    Fixes pinned branch state not restoring on graph reopen  (#5139, #5181)

--- src/webviews/plus/graph/graphWebview.ts
+++ src/webviews/plus/graph/graphWebview.ts
@@ -534,7 +534,7 @@ export class GraphWebviewProvider implements WebviewProvider<State, State, Graph
 		return (this._graphRowProcessor ??= new GlGraphRowProcessor(
 			this.container,
 			uri => this.host.asWebviewUri(uri),
-			() => this.getFiltersByRepo(this._graph?.repoPath)?.pinnedRef?.id,
+			() => this.getFiltersByRepo(this.repository?.path ?? this._graph?.repoPath)?.pinnedRef?.id,
 		));
 	}
 
@@ -5365,6 +5365,7 @@ export class GraphWebviewProvider implements WebviewProvider<State, State, Graph
 					}
 
 					void this.notifyDidChangeRefsVisibility();
+					void this.notifyDidChangePinnedRef();
 					void this.notifyDidChangeRows(selectionChanged);
 					this.notifySidebarInvalidated();
 				} catch {}
