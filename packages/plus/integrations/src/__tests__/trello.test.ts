a7bdf276d71fe88fded2121acbe8dbf9326901c2
Julian Mesa <julian.mesa@gitkraken.com>
2026-07-24 16:56:22 +0200

    fix(integrations): harden Kepler provider backend contracts

--- packages/plus/integrations/src/__tests__/trello.test.ts
+++ packages/plus/integrations/src/__tests__/trello.test.ts
@@ -4,7 +4,7 @@ import type { Issue } from '@gitlens/git/models/issue.js';
 import type { ProviderAuthenticationSession, TokenWithInfo } from '../authentication/models.js';
 import { toTokenWithInfo } from '../authentication/models.js';
 import { IssuesCloudHostIntegrationId } from '../constants.js';
-import { createIntegrationManager } from '../index.js';
+import { createIntegrationService as createIntegrationManager } from '../integrationService.js';
 import type { IssuesIntegration } from '../models/issuesIntegration.js';
 import type { ProviderIssue } from '../providers/models.js';
 import {
